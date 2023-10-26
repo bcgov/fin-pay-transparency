e55d99b21ea789ae16db8bceccf114cce9ab42e0
Jerek Mehrer <Jerek.Mehrer@gov.bc.ca>
2023-10-26 09:38:08 -0700

    GEO-58: Create dashboard

---
+++ frontend/src/components/Dashboard.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-container class="d-flex justify-center">
+    <v-btn to="GenerateReport">Generate Pay Transparency Report</v-btn>
+    <v-btn>View Generated Reports</v-btn>
+  </v-container>
+</template>
