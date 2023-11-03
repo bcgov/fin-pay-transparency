028ad0d73e99a3ca3958a63760b9ea1700c38fb8
Brock Anderson <brock@bandersgeo.ca>
2023-11-03 10:40:04 -0700

    added a new test for the date fields

--- frontend/src/components/__tests__/InputForm.spec.ts
+++ frontend/src/components/__tests__/InputForm.spec.ts
@@ -4,7 +4,7 @@ import { createVuetify } from "vuetify";
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import InputForm from '../InputForm.vue'
-
+import moment from 'moment';
 
 describe("InputForm", () => {
   let wrapper = null;
@@ -43,6 +43,20 @@ describe("InputForm", () => {
     expect(wrapper.find("#csvFile").attributes("type")).toBe("file");
     expect(wrapper.find("#csvFile").attributes("accept")).toBe(".csv");
     
+  })
+
+  it('Setting start date causes end date to default to one year later', async () => { 
+
+    const startDateComponent = wrapper.findComponent({ref: 'startDate'})
+    const endDateComponent = wrapper.findComponent({ref: 'endDate'})
+
+    const startDate = moment().subtract(1, "years").format("yyyy-MM");
+    const expectedEndDate = moment().subtract(1, "months").format("yyyy-MM");
+    await startDateComponent.setValue(startDate);
+    
+    expect(wrapper.vm.$data.startDate).toBe(startDate)
+    expect(wrapper.vm.$data.endDate).toBe(expectedEndDate)
+
   })
 
 })
