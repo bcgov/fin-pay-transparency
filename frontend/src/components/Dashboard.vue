ff52751171825a263a59c1bbc78c5e026ac4ef05
Brock Anderson <brock@bandersgeo.ca>
2023-11-06 12:04:46 -0800

    Mocked up a new layout for the dashboard.  It includes sample text and placeholder panes for some content

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,6 +1,72 @@
 <template>
-  <v-container class="d-flex justify-center">
-    <v-btn to="GenerateReport">Generate Pay Transparency Report</v-btn>
-    <v-btn>View Generated Reports</v-btn>
+  <v-container class="d-flex justify-center fill-height" fluid>
+
+    <v-row class="h-100">
+      <v-col>
+
+        <v-card min-height="70%">
+          <v-card-text>
+            <p class="mb-2">
+              The tool will help you generate a pay transparency report for you company.
+            </p>
+            <p class="mb-2">
+              To begin, organize your pay transparency data into a comma-separated value (CSV) file as described
+              <u>here</u>.
+            </p>
+            <p class="mb-4">
+              Then click the button below to access a submission
+              form, where you'll be asked to enter information about your company and upload your CSV file.
+            </p>
+            <v-btn color="secondary" to="GenerateReport">Generate Pay Transparency Report</v-btn>
+          </v-card-text>
+        </v-card>
+
+      </v-col>
+      <v-col>
+        <v-card min-height="70%">
+          <v-card-text>
+            No generated reports yet.
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="3">
+        <v-row>
+          <v-col>
+
+            <v-card>
+              <v-toolbar color='secondary'>
+                <v-toolbar-title>Sample CSV</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <p class="mb-4">
+                  View a sample comma-separated value (CSV) file showing the required format for pay transparency data
+                  uploads.
+                </p>
+                <v-btn color="secondary">Download sample CSV</v-btn>
+              </v-card-text>
+            </v-card>
+
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+
+            <v-card>
+              <v-toolbar color='secondary'>
+                <v-toolbar-title>Updates</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <p class="mb-4">
+                  There are no updates at this time
+                </p>
+              </v-card-text>
+            </v-card>
+
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+
+
   </v-container>
 </template>
