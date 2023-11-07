9565bca334fc537c42f989a7cf7f4692efd8d3d5
Banders Geospatial Systems Ltd <brock@bandersgeo.ca>
2023-11-06 16:42:41 -0800

    Input Form Screen UI Page (#47)

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container class="d-flex justify-center">
-    <v-btn to="GenerateReport">Generate Pay Transparency Report</v-btn>
+    <v-btn to="inputForm">Input Form</v-btn>
     <v-btn>View Generated Reports</v-btn>
   </v-container>
 </template>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,16 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue()
+  ],
+  test: {
+    environment: "jsdom",
+    deps: {
+      inline: ["vuetify"],
+    },
+    globals: true,
+  },
   server: {
     port: 8081,
     proxy: {
