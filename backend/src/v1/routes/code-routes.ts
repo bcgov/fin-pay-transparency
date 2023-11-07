69db42f65849f76384a47c8d873296f653e726ed
Brock Anderson <brock@bandersgeo.ca>
2023-11-07 15:24:58 -0800

    changed export format of codeRouter

--- backend/src/v1/routes/code-routes.ts
+++ backend/src/v1/routes/code-routes.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-import passport from 'passport';
 import { auth } from "../services/auth-service";
-import { codeService } from '../services/code-service';
 
 const isValidBackendToken = auth.isValidBackendToken();
-const router = express.Router();
-
-router.get('/employee-count-range ', passport.authenticate('jwt', { session: false }), isValidBackendToken, async (req, res) => {
-  res.sendStatus(200).json(codeService.getAllEmployeeCountRanges);
+const codeRouter = express.Router();
+//passport.authenticate('jwt', { session: false }), isValidBackendToken,
+codeRouter.get('/employee-count-range ', async (req, res) => {
+  console.log("ASDFS");
+  res.sendStatus(200); //.json(codeService.getAllEmployeeCountRanges);
 });
 
-export = router;
+export { codeRouter };
+
