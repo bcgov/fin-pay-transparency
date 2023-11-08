b144aa07f233d209d1600511fd5c2c0c3c1b3e69
Brock Anderson <brock@bandersgeo.ca>
2023-11-08 13:41:59 -0800

    fixed merge conflicts

--- backend/src/v1/routes/code-routes.ts
+++ backend/src/v1/routes/code-routes.ts
@@ -1,22 +1,11 @@
 import express from 'express';
 import passport from 'passport';
 import { auth } from "../services/auth-service";
-<<<<<<< HEAD
 import { codeService } from '../services/code-service';
-=======
-import { codeService } from "../services/code-service";
->>>>>>> bb0ba5ad6a7c37899f2a4d0e4842636583f0755e
 
 const isValidBackendToken = auth.isValidBackendToken();
 const router = express.Router();
 
-<<<<<<< HEAD
-router.get('/employee-count-range ', passport.authenticate('jwt', { session: false }), isValidBackendToken, async (req, res) => {
-  res.sendStatus(200).json(codeService.getAllEmployeeCountRanges);
-});
-
-export = router;
-=======
 router.get('/employee-count-ranges', passport.authenticate('jwt', { session: false }), isValidBackendToken, async (req, res) => {
   const body = await codeService.getAllEmployeeCountRanges()
   res.status(200).json(body);
@@ -24,4 +13,3 @@ router.get('/employee-count-ranges', passport.authenticate('jwt', { session: fal
 
 export = router;
 
->>>>>>> bb0ba5ad6a7c37899f2a4d0e4842636583f0755e
