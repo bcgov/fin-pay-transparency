ccd366dd726c45ae2d31b8b572c856bce63e9685
Brock Anderson <brock@bandersgeo.ca>
2023-11-09 17:26:29 -0800

    added a new 'store' to manage retrieval and storage of static content (employee count ranges and naics codes) from the backend.

---
+++ frontend/src/store/modules/codeStore.ts
@@ -0,0 +1,52 @@
+import { defineStore, storeToRefs } from 'pinia';
+import { ref, watch } from 'vue';
+import ApiService from '../../common/apiService.js';
+import { authStore } from './auth.js';
+
+/*
+The CodeStore houses static data that is ultimately to be used to populate 
+lists of options in the UI.  It is responsible for fetching 
+those data from the backend and sharing them via a reactive interface.
+*/
+export const useCodeStore = defineStore('code', () => {
+
+  const auth = authStore();
+  const { isAuthenticated } = storeToRefs(auth)
+
+  const employeeCountRanges = ref([]);
+  const naicsCodes = ref([]);
+
+  const setEmployeeCountRanges = (val) => {
+    employeeCountRanges.value = val;
+  }
+  const setNaicsCodes = (val) => {
+    naicsCodes.value = val;
+  }
+
+  const fetchAllCodes = async () => {
+    const employeeCountRanges = await ApiService.getEmployeeCountRanges()
+    setEmployeeCountRanges(employeeCountRanges)
+
+    const naicsCodes = await ApiService.getNaicsCodes()
+    setNaicsCodes(naicsCodes)
+  }
+
+  // Watch for changes to the isAuthenticated property in the AuthStore.  When set 
+  // to true initiate API calls to fetch static data from the backend. (The API 
+  // calls require a token showing the user is authenticated.)
+  watch(
+    isAuthenticated,
+    async (isAuthenticated) => {
+      if (isAuthenticated) {
+        fetchAllCodes();
+      }
+    },
+    { immediate: true }
+  )
+
+  // Return the public interface for the store
+  return {
+    employeeCountRanges,
+    naicsCodes
+  }
+})
