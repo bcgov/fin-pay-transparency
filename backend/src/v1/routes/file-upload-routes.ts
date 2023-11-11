751e4c707c46fe146e62470d07fbaac318c2132b
Brock Anderson <brock@bandersgeo.ca>
2023-11-10 17:17:23 -0800

    work-in-progress commit to start unpacking and validating the submission

--- backend/src/v1/routes/file-upload-routes.ts
+++ backend/src/v1/routes/file-upload-routes.ts
@@ -1,16 +1,24 @@
-import {auth} from "../services/auth-service";
 import express from "express";
 import passport from 'passport';
+import { auth } from "../services/auth-service";
+import { getCompanies } from '../services/file-upload-service';
+const multer = require('multer');
+const upload = multer();
 
 const fileUploadRouter = express.Router();
-import {getCompanies} from '../services/file-upload-service';
-fileUploadRouter.post("/",passport.authenticate('jwt', {session: false}, undefined), auth.isValidBackendToken(), async (req, res) => {
-  //await saveFileUpload(req.body);
-  console.log(req.body);
-  res.sendStatus(200);
-});
-fileUploadRouter.get("/",passport.authenticate('jwt', {session: false}, undefined), auth.isValidBackendToken(), async (req, res) => {
+fileUploadRouter.post("/",
+  passport.authenticate('jwt', { session: false }, undefined),
+  auth.isValidBackendToken(),
+  upload.single("file"),
+  async (req, res) => {
+    //await saveFileUpload(req.body);
+    console.log("submission body:");
+    console.log(req.body);
+    res.sendStatus(200);
+  });
+fileUploadRouter.get("/", passport.authenticate('jwt', { session: false }, undefined), auth.isValidBackendToken(), async (req, res) => {
   const companies = await getCompanies();
   res.status(200).json(companies);
 });
-export {fileUploadRouter};
+export { fileUploadRouter };
+
