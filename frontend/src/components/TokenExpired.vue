5736e8cbe1982f5d8c87f3d9f8a4c06971b8be41
Sukanya Rath <98050194+sukanya-rath@users.noreply.github.com>
2023-11-27 18:29:09 -0800

    Fix/login on token expire (#73)
    Signed-off-by: Sukanya Rath

--- frontend/src/components/TokenExpired.vue
+++ frontend/src/components/TokenExpired.vue
@@ -5,13 +5,37 @@
         <v-alert dense outlined class="bootstrap-error mb-3">
           Your session has expired.
         </v-alert>
-        <v-btn color="primary" to="Login">Login again</v-btn>
+        <v-btn color="primary" :href="authRoutes.LOGIN_BCEID">Login Again</v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
-<script lang="ts">
+<script >
+import { authStore } from '../store/modules/auth';
+import { mapState } from 'pinia';
+import { AuthRoutes } from '../utils/constant';
+
+export default {
+  name: 'TokenExpired',
+  components: {
+
+  },
+  data() {
+    return {
+      appTitle: 'Pay Transparency Reporting',
+      authRoutes: AuthRoutes
+    };
+  },
+  computed: {
+    ...mapState(authStore, ['isAuthenticated']),
+  },
+  methods: {
+    clearStorage() {
+      authStore().setJwtToken();
+    }
+  }
+};
 </script>
 
 <styles>
