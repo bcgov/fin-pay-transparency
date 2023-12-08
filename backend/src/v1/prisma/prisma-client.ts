cb9ebb07769d4c54ea45c10ccdfc9a31be2601cc
Sukanya Rath <sukanyarath1987@gmail.com>
2023-12-08 11:07:45 -0800

    remove pgbouncer,final attempt.
    Signed-off-by: Sukanya Rath <sukanyarath1987@gmail.com>

--- backend/src/v1/prisma/prisma-client.ts
+++ backend/src/v1/prisma/prisma-client.ts
@@ -11,7 +11,7 @@ const DB_NAME = process.env.POSTGRESQL_DATABASE || 'postgres';
 const DB_SCHEMA = process.env.DB_SCHEMA || 'pay_transparency';
 if (!prisma) {
   const datasourceUrl = `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`;
-  logger.info(`Connecting to ${datasourceUrl}`);
+  logger.silly(`Connecting to ${datasourceUrl}`);
   prisma = new PrismaClient({
     log: ['query', 'info', "error", "warn"],
     errorFormat: 'pretty',
