0db3080a20018b5aff4fa9d0ef4634f859e1b222
Sukanya Rath <sukanyarath1987@gmail.com>
2023-12-07 21:06:33 -0800

    add encodeURI to db password.
    Signed-off-by: Sukanya Rath <sukanyarath1987@gmail.com>

--- backend/src/v1/prisma/prisma-client.ts
+++ backend/src/v1/prisma/prisma-client.ts
@@ -1,3 +1,5 @@
+import {logger} from "../../logger";
+
 let prisma: PrismaClient;
 import {PrismaClient} from '@prisma/client';
 
@@ -7,14 +9,9 @@ const DB_PWD = encodeURI(process.env.POSTGRESQL_PASSWORD || 'postgres');
 const DB_PORT = process.env.POSTGRESQL_PORT || 5432;
 const DB_NAME = process.env.POSTGRESQL_DATABASE || 'postgres';
 const DB_SCHEMA = process.env.DB_SCHEMA || 'pay_transparency';
-const PG_BOUNCER_ENABLED = process.env.PG_BOUNCER_ENABLED || 'false';
 if (!prisma) {
-  let datasourceUrl = `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}`;
-  if(PG_BOUNCER_ENABLED === 'true') {
-    datasourceUrl += '&pgbouncer=true';
-  }else{
-    datasourceUrl += '&connection_limit=5';
-  }
+  const datasourceUrl = `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`;
+  logger.info(`Connecting to ${datasourceUrl}`);
   prisma = new PrismaClient({
     log: ['query', 'info', "error", "warn"],
     errorFormat: 'pretty',
