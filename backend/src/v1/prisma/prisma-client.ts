988ed6672c2bd1b1da26a2b004ef7fe9159c1883
Sukanya Rath <sukanyarath1987@gmail.com>
2023-12-07 18:08:38 -0800

    add encodeURI to db password.
    Signed-off-by: Sukanya Rath <sukanyarath1987@gmail.com>

--- backend/src/v1/prisma/prisma-client.ts
+++ backend/src/v1/prisma/prisma-client.ts
@@ -7,12 +7,18 @@ const DB_PWD = encodeURI(process.env.POSTGRESQL_PASSWORD || 'postgres');
 const DB_PORT = process.env.POSTGRESQL_PORT || 5432;
 const DB_NAME = process.env.POSTGRESQL_DATABASE || 'postgres';
 const DB_SCHEMA = process.env.DB_SCHEMA || 'pay_transparency';
+const PG_BOUNCER_ENABLED = process.env.PG_BOUNCER_ENABLED || 'false';
 if (!prisma) {
+  let datasourceUrl = `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}`;
+  if(PG_BOUNCER_ENABLED === 'true') {
+    datasourceUrl += '&pgbouncer=true';
+  }else{
+    datasourceUrl += '&connection_limit=5';
+  }
   prisma = new PrismaClient({
     log: ['query', 'info', "error", "warn"],
     errorFormat: 'pretty',
-    datasourceUrl: `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`
-
+    datasourceUrl: datasourceUrl
   });
 }
 
