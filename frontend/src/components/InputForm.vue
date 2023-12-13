b7395b631e3daf8539b3dfe1dbc4695b83d13ec7
Brock Anderson <brock@bandersgeo.ca>
2023-12-12 20:34:51 -0800

    changes to show the draft report inline below the input form (this is a temporary placement while the report is being developed)

--- frontend/src/components/InputForm.vue
+++ frontend/src/components/InputForm.vue
@@ -1,9 +1,9 @@
 <template>
   <v-container class="d-flex justify-center">
 
-    <v-form ref="inputForm">
-      <v-row class=" d-flex justify-center">
-        <v-col xs="12" sm="10" md="8">
+    <v-form ref="inputForm" class="w-100">
+      <v-row class=" d-flex justify-center w-100">
+        <v-col xs="12" sm="10" md="8" class="w-100">
 
           <v-row class="pt-7">
             <v-col cols="12">
@@ -12,24 +12,24 @@
           </v-row>
 
           <!-- timeline -->
-          <v-row class="pt-7 mb-4 d-flex justify-center">
-            <v-col cols="10">
+          <v-row class="pt-7 mb-4 d-flex justify-center w-100">
+            <v-col cols="10" class="w-100">
 
               <v-row>
                 <v-col class="d-flex-col justify-center align-center">
-                  <div class="circle">1</div>
+                  <div class="circle" :class="{ disabled: stage != 'UPLOAD' }">1</div>
                 </v-col>
                 <v-col class="d-flex justify-center align-center">
                   <div class="dash disabled"></div>
                 </v-col>
                 <v-col class="d-flex justify-center align-center">
-                  <div class="circle disabled">2</div>
+                  <div class="circle" :class="{ disabled: stage != 'REVIEW' }">2</div>
                 </v-col>
                 <v-col class="d-flex justify-center align-center">
                   <div class="dash disabled"></div>
                 </v-col>
                 <v-col class="d-flex justify-center align-center">
-                  <div class="circle disabled">3</div>
+                  <div class="circle disabled" :class="{ disabled: stage != 'GENERATE' }">3</div>
                 </v-col>
               </v-row>
 
@@ -52,170 +52,178 @@
             </v-col>
           </v-row>
 
-          <v-row class="d-flex justify-start mt-12" dense>
-            <v-col cols="12">
-              <h2 class="text-center">Your Company Information</h2>
-            </v-col>
+          <div v-if="stage !== 'ANY'">
 
-            <v-col cols="12">
-              <v-text-field id="companyName" ref="companyName" v-model="companyName" label="Company Name"
-                :rules="requiredRules" required disabled></v-text-field>
-            </v-col>
+            <v-row class="d-flex justify-start mt-12" dense>
+              <v-col cols="12">
+                <h2 class="text-center">Your Company Information</h2>
+              </v-col>
 
-            <v-col cols="12">
-              <v-text-field id="companyAddress" ref="companyAddress" v-model="companyAddress" :rules="requiredRules"
-                label="Company Address" required disabled></v-text-field>
-            </v-col>
+              <v-col cols="12">
+                <v-text-field id="companyName" ref="companyName" v-model="companyName" label="Company Name"
+                  :rules="requiredRules" required disabled></v-text-field>
+              </v-col>
 
-            <v-col cols="12" class="d-flex">
-              <v-autocomplete id="naicsCode" ref="naicsCode" v-model="naicsCode" :rules="requiredRules"
-                :items="naicsCodes" :item-title="n => `${n.naics_code} - ${n.naics_label}`" item-value="naics_code"
-                label="NAICS Code" required></v-autocomplete>
-              <v-icon color="error" icon="mdi-asterisk" size="x-small" v-if="!naicsCode"></v-icon>
-            </v-col>
+              <v-col cols="12">
+                <v-text-field id="companyAddress" ref="companyAddress" v-model="companyAddress" :rules="requiredRules"
+                  label="Company Address" required disabled></v-text-field>
+              </v-col>
 
-            <v-col cols="12" class="d-flex">
-              <v-select id="employeeCountRange" ref="employeeCountRange" v-model="employeeCountRange"
-                :rules="requiredRules" label="Employee Count Range" :items="employeeCountRanges"
-                item-title="employee_count_range" item-value="employee_count_range_id" required></v-select>
-              <v-icon color="error" icon="mdi-asterisk" size="x-small" v-if="!employeeCountRange"></v-icon>
-            </v-col>
+              <v-col cols="12" class="d-flex">
+                <v-autocomplete id="naicsCode" ref="naicsCode" v-model="naicsCode" :rules="requiredRules"
+                  :items="naicsCodes" :item-title="n => `${n.naics_code} - ${n.naics_label}`" item-value="naics_code"
+                  label="NAICS Code" required></v-autocomplete>
+                <v-icon color="error" icon="mdi-asterisk" size="x-small" v-if="!naicsCode"></v-icon>
+              </v-col>
 
-            <v-col cols="6" class="d-flex">
-              <VueDatePicker id="startDate" ref="startDate" v-model="startDate" model-type="yyyy-MM" month-picker
-                auto-apply format="MMMM yyyy" placeholder="Start Date" input-class-name="datepicker-input"
-                :min-date="minStartDate" :max-date="maxStartDate" prevent-min-max-navigation
-                :action-row="{ showSelect: false, showCancel: false, showNow: false, showPreview: false }" />
-              <v-icon color="error" icon="mdi-asterisk" size="x-small" v-if="!startDate"></v-icon>
-            </v-col>
+              <v-col cols="12" class="d-flex">
+                <v-select id="employeeCountRange" ref="employeeCountRange" v-model="employeeCountRange"
+                  :rules="requiredRules" label="Employee Count Range" :items="employeeCountRanges"
+                  item-title="employee_count_range" item-value="employee_count_range_id" required></v-select>
+                <v-icon color="error" icon="mdi-asterisk" size="x-small" v-if="!employeeCountRange"></v-icon>
+              </v-col>
 
-            <v-col cols="6" class="d-flex">
-              <VueDatePicker id="endDate" ref="endDate" v-model="endDate" model-type="yyyy-MM" month-picker auto-apply
-                format="MMMM yyyy" placeholder="End Date" input-class-name="datepicker-input" :min-date="minEndDate"
-                :max-date="maxEndDate" prevent-min-max-navigation
-                :action-row="{ showSelect: false, showCancel: false, showNow: false, showPreview: false }" />
-              <v-icon color="error" icon="mdi-asterisk" size="x-small" v-if="!endDate"></v-icon>
-            </v-col>
+              <v-col cols="6" class="d-flex">
+                <VueDatePicker id="startDate" ref="startDate" v-model="startDate" model-type="yyyy-MM" month-picker
+                  auto-apply format="MMMM yyyy" placeholder="Start Date" input-class-name="datepicker-input"
+                  :min-date="minStartDate" :max-date="maxStartDate" prevent-min-max-navigation
+                  :action-row="{ showSelect: false, showCancel: false, showNow: false, showPreview: false }" />
+                <v-icon color="error" icon="mdi-asterisk" size="x-small" v-if="!startDate"></v-icon>
+              </v-col>
 
-            <v-col cols="12" class="mt-6">
-              <p class="text-subtitle-2">Please note any limitations, dependencies, or constraints with the payroll data
-                which
-                will appear at the
-                bottom of the report</p>
-              <v-textarea id="dataConstraints" v-model="dataConstraints" label="Data Constraints" maxlength="3000"
-                clearable>
-                <template v-slot:details>
-                </template>
-              </v-textarea>
-            </v-col>
+              <v-col cols="6" class="d-flex">
+                <VueDatePicker id="endDate" ref="endDate" v-model="endDate" model-type="yyyy-MM" month-picker auto-apply
+                  format="MMMM yyyy" placeholder="End Date" input-class-name="datepicker-input" :min-date="minEndDate"
+                  :max-date="maxEndDate" prevent-min-max-navigation
+                  :action-row="{ showSelect: false, showCancel: false, showNow: false, showPreview: false }" />
+                <v-icon color="error" icon="mdi-asterisk" size="x-small" v-if="!endDate"></v-icon>
+              </v-col>
 
-            <v-col cols="12" class="">
-              <p class="text-subtitle-2">Other comments</p>
-              <v-textarea id="comments" v-model="comments" label="Contextual Info/Comments" clearable>
-              </v-textarea>
-            </v-col>
+              <v-col cols="12" class="mt-6">
+                <p class="text-subtitle-2">Please note any limitations, dependencies, or constraints with the payroll data
+                  which
+                  will appear at the
+                  bottom of the report</p>
+                <v-textarea id="dataConstraints" v-model="dataConstraints" label="Data Constraints" maxlength="3000"
+                  clearable>
+                  <template v-slot:details>
+                  </template>
+                </v-textarea>
+              </v-col>
 
-            <v-col cols="12">
-              <h3 class="mb-2">File Upload</h3>
-              <p class="mb-4">
-                To proceed, upload your employee data in comma-separated value (CSV) format. Ensure the CSV file follows
-                the provided CSV template
-                (<u>bc-pay-transparency-tool-data-template.csv</u>) for accurate processing.
-              </p>
-              <v-sheet class="pa-5" style="border-style: dashed; border: 3px dashed #666666; border-radius: 10px;">
-                <div class="d-flex">
-                  <v-file-input id="csvFile" v-model="uploadFileValue" color="#003366" :accept="fileAccept"
-                    hint="Select a CSV file" :error-messages="fileInputError" placeholder="Select a CSV file"
-                    :rules="requiredRules" />
-                  <v-icon color="error" icon="mdi-asterisk" size="x-small" v-if="!uploadFileValue"></v-icon>
-                </div>
+              <v-col cols="12" class="">
+                <p class="text-subtitle-2">Other comments</p>
+                <v-textarea id="comments" v-model="comments" label="Contextual Info/Comments" clearable>
+                </v-textarea>
+              </v-col>
 
-                <p class="d-flex justify-center">
-                  Supported format: CSV. Maximum file size: 8MB.
+              <v-col cols="12">
+                <h3 class="mb-2">File Upload</h3>
+                <p class="mb-4">
+                  To proceed, upload your employee data in comma-separated value (CSV) format. Ensure the CSV file follows
+                  the provided CSV template
+                  (<u>bc-pay-transparency-tool-data-template.csv</u>) for accurate processing.
                 </p>
-              </v-sheet>
-            </v-col>
+                <v-sheet class="pa-5" style="border-style: dashed; border: 3px dashed #666666; border-radius: 10px;">
+                  <div class="d-flex">
+                    <v-file-input id="csvFile" v-model="uploadFileValue" color="#003366" :accept="fileAccept"
+                      hint="Select a CSV file" :error-messages="fileInputError" placeholder="Select a CSV file"
+                      :rules="requiredRules" />
+                    <v-icon color="error" icon="mdi-asterisk" size="x-small" v-if="!uploadFileValue"></v-icon>
+                  </div>
 
-          </v-row>
+                  <p class="d-flex justify-center">
+                    Supported format: CSV. Maximum file size: 8MB.
+                  </p>
+                </v-sheet>
+              </v-col>
 
-          <v-row class="mt-6">
-            <v-col cols="12" class="d-flex justify-center" v-if="!areRequiredFieldsComplete">
-              <v-icon color="error" icon="mdi-asterisk" size="x-small"></v-icon>
-              Please complete all required fields
-            </v-col>
-            <v-col cols="12" class="d-flex justify-center">
+            </v-row>
 
-              <primary-button id="submitButton" :disabled="!areRequiredFieldsComplete" :loading="isProcessing"
-                text="Submit" :click-action="submit" />
-            </v-col>
-          </v-row>
+            <v-row class="mt-6">
+              <v-col cols="12" class="d-flex justify-center" v-if="!areRequiredFieldsComplete">
+                <v-icon color="error" icon="mdi-asterisk" size="x-small"></v-icon>
+                Please complete all required fields
+              </v-col>
+              <v-col cols="12" class="d-flex justify-center">
 
-          <v-row class="mt-3">
-            <v-col>
-              <v-alert v-if="alertMessage" dense outlined dismissible :class="alertType" class="mb-3">
-                {{ alertMessage }}
-              </v-alert>
-            </v-col>
-          </v-row>
+                <primary-button id="submitButton" :disabled="!areRequiredFieldsComplete" :loading="isProcessing"
+                  text="Submit" :click-action="submit" />
+              </v-col>
+            </v-row>
 
-          <v-row class="mt-3" v-if="submissionErrors">
-            <v-col>
-              <v-alert dense outlined dismissible class="bootstrap-error mb-3">
-                <h4 class="mb-3">The submission contains errors which must be corrected.</h4>
+            <v-row class="mt-3">
+              <v-col>
+                <v-alert v-if="alertMessage" dense outlined dismissible :class="alertType" class="mb-3">
+                  {{ alertMessage }}
+                </v-alert>
+              </v-col>
+            </v-row>
 
-                <!-- general errors related to the submission (either with the 
-                  form fields or with the file itself) -->
-                <v-table v-if="submissionErrors?.generalErrors" density="compact">
-                  <tbody>
-                    <tr v-for="generalError in submissionErrors.generalErrors">
-                      <td class="text-left">
-                        {{ generalError }}
-                      </td>
-                    </tr>
-                  </tbody>
-                </v-table>
+            <v-row class="mt-3" v-if="submissionErrors">
+              <v-col>
+                <v-alert dense outlined dismissible class="bootstrap-error mb-3">
+                  <h4 class="mb-3">The submission contains errors which must be corrected.</h4>
 
-                <!-- general errors related to contents of the file -->
-                <v-table v-if="submissionErrors?.fileErrors?.generalErrors" density="compact">
-                  <tbody>
-                    <tr v-for="generalError in submissionErrors.fileErrors.generalErrors">
-                      <td class="text-left">
-                        {{ generalError }}
-                      </td>
-                    </tr>
-                  </tbody>
-                </v-table>
-
-                <!-- errors related to the content of specific lines in the file -->
-                <div v-if="submissionErrors?.fileErrors?.lineErrors">
-                  <h4 class="mb-3">Please review the following lines from the uploaded file:
-                  </h4>
-                  <v-table density="compact">
-                    <thead>
-                      <tr>
-                        <th class="text-left">
-                          Line
-                        </th>
-                        <th class="text-left">
-                          Problem(s)
-                        </th>
-                      </tr>
-                    </thead>
+                  <!-- general errors related to the submission (either with the 
+                    form fields or with the file itself) -->
+                  <v-table v-if="submissionErrors?.generalErrors" density="compact">
                     <tbody>
-                      <tr v-for="lineError in submissionErrors.fileErrors.lineErrors" :key="lineError.lineNum">
-                        <td class="text-left">{{ lineError.lineNum }}</td>
-                        <td class="text-left ">
-                          <span v-for="errMsg in lineError.errors" class="mr-2">
-                            {{ errMsg }}
-                          </span>
+                      <tr v-for="generalError in submissionErrors.generalErrors">
+                        <td class="text-left">
+                          {{ generalError }}
                         </td>
                       </tr>
                     </tbody>
                   </v-table>
-                </div>
-              </v-alert>
-            </v-col>
-          </v-row>
+
+                  <!-- general errors related to contents of the file -->
+                  <v-table v-if="submissionErrors?.fileErrors?.generalErrors" density="compact">
+                    <tbody>
+                      <tr v-for="generalError in submissionErrors.fileErrors.generalErrors">
+                        <td class="text-left">
+                          {{ generalError }}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </v-table>
+
+                  <!-- errors related to the content of specific lines in the file -->
+                  <div v-if="submissionErrors?.fileErrors?.lineErrors">
+                    <h4 class="mb-3">Please review the following lines from the uploaded file:
+                    </h4>
+                    <v-table density="compact">
+                      <thead>
+                        <tr>
+                          <th class="text-left">
+                            Line
+                          </th>
+                          <th class="text-left">
+                            Problem(s)
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr v-for="lineError in submissionErrors.fileErrors.lineErrors" :key="lineError.lineNum">
+                          <td class="text-left">{{ lineError.lineNum }}</td>
+                          <td class="text-left ">
+                            <span v-for="errMsg in lineError.errors" class="mr-2">
+                              {{ errMsg }}
+                            </span>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </v-table>
+                  </div>
+                </v-alert>
+              </v-col>
+            </v-row>
+
+          </div>
+
+          <div v-if="stage == 'REVIEW'" v-html="draftReport">            
+            
+          </div>
 
         </v-col>
       </v-row>
@@ -279,7 +287,9 @@ export default {
     fileInputError: [],
     alertMessage: null,
     alertType: null,
-    submissionErrors: null as SubmissionErrors | null
+    submissionErrors: null as SubmissionErrors | null,
+    draftReport: null,
+    stage: "UPLOAD"
   }),
   methods: {
     setSuccessAlert(alertMessage) {
@@ -309,7 +319,9 @@ export default {
           file: this.uploadFileValue[0],
         }
         const response = await ApiService.postSubmission(formData);
-        console.log(response);
+        console.log(response)
+        this.draftReport = response;
+        this.stage = "REVIEW";
         this.setSuccessAlert('Submission received.');
         this.setErrorAlert(null);
         this.isProcessing = false;
@@ -342,7 +354,7 @@ export default {
           throw e;
         }
       }
-    },
+    }    
   },
   watch: {
     naicsCodes(val) {
