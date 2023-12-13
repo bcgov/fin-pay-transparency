60562b7c9d279166550c926d6f29101e525d285c
Brock Anderson <brock@bandersgeo.ca>
2023-12-13 10:28:23 -0800

    changed path to template

--- backend/src/v1/services/report-service.ts
+++ backend/src/v1/services/report-service.ts
@@ -8,8 +8,8 @@ import prisma from "../prisma/prisma-client";
 import { CALCULATION_CODES } from './report-calc-service';
 import { utils } from './utils-service';
 
-const REPORT_TEMPLATE = resolve("./src/templates/", "report.template.html");
-const REPORT_TEMPLATE_SCRIPT = resolve("./src/templates/", "report.script.js");
+const REPORT_TEMPLATE = resolve("./templates", "report.template.html");
+const REPORT_TEMPLATE_SCRIPT = resolve("./templates", "report.script.js");
 const GENDER_CHART_LABELS = {
   MALE: "Male",
   FEMALE: "Female",
