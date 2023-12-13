502f0029ff9408ce9f26f03f618ac3007981ff97
Brock Anderson <brock@bandersgeo.ca>
2023-12-13 11:50:41 -0800

    changed puppeteer cache dir

--- backend/puppeteer.config.cjs
+++ backend/puppeteer.config.cjs
@@ -5,6 +5,5 @@ const {resolve, join} = require('path');
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: resolve('.', '.cache', 'puppeteer')
-  //cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
