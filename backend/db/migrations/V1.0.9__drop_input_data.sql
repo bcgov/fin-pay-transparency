6f1471c8e6452510543ed6a969d0628cb73b49ba
andybcgov <118761047+andybcgov@users.noreply.github.com>
2023-12-21 12:17:41 -0800

    drop data input table and generate schema.prisma

--- backend/db/migrations/V1.0.9__drop_input_data.sql
+++ backend/db/migrations/V1.0.9__drop_input_data.sql
@@ -3,5 +3,3 @@ set search_path to pay_transparency;
 delete from pay_transparency_input_data;
 alter table pay_transparency_input_data drop constraint input_data_report_id_fk;
 drop table pay_transparency_input_data;
-
-select * from pay_transparency_input_data;
