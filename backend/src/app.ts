e1829da1afee0ff105533ae6ea10c3ca105d2db6
Sukanya Rath <98050194+sukanya-rath@users.noreply.github.com>
2024-01-23 14:08:52 -0800

    Add missing import

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 import bodyParser from 'body-parser';
 import cors from 'cors';
