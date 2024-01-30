b32bf2ff7192d15f6a866d7c744102288d1d8856
andybcgov <118761047+andybcgov@users.noreply.github.com>
2024-01-30 10:49:03 -0800

    stash

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,16 +1,24 @@
 <template>
-  <v-container class="unbastardized">
+  <v-container class="undized">
     <v-row no-gutters>
       <v-col cols="12">
         <v-container class="justify-center">
           <v-row no-gutters>
             <v-col>
-              <p class="text-welcome mt-14 mb-8">
-                Pay Transparency Reporting tool consectetur adipiscing elit. Etiam dictum ultrices purus, ac malesuada leo congue vel.
+
+              <p class="text-welcome mt-14 mb-8 centered-text">
+                Welcome to the Pay Transparency Reporting Tool
               </p>
               <p class="text-subtitle-2 mb-10">
-                Introducing the Pay Transparency Reporting tool, onsectetur adipiscing elit. Ut fermentum tortor ut ultricies placerat. Nam eu leo lacus. Quisque id tempor arcu. Ut fermentum tortor ut ultricies placer, visit <u>Pay Transparency Laws in B.C.</u>
+                In British Columbia, if you are a provincially regulated employer above a certain size, you are required to complete and post a pay transparency report on all your B.C. employees by November 1st of each year.  This portal enables you to easily create a pay transparency report as required under the <a target="_blank" href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/23018">Pay Transparency Act</a> and <a target="_blank" href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/225_2023">Regulation</a>. 
+                <ul class="ml-8 mt-4 mb-4">
+                  <li>2024: all employers with 1,000 employees or more will be required to begin posting annual pay transparency reports</li>
+                  <li>2025: all employers with 300 employees or more will be required to begin posting annual pay transparency reports</li>
+                  <li>2026: all employers with 50 employees or more will be required to begin posting annual pay transparency reports</li>
+                </ul>
+                Below is an overview of the three steps required to produce your pay transparency report. For additional information please visit <a target="_blank" href="https://www2.gov.bc.ca/gov/content/gender-equity/pay-transparency-laws-in-bc">Pay Transparency Laws in B.C.</a>
               </p>
+              <p class="text-caption">This application does not collect, record or publish personal information.</p>
               <v-btn
                 id="login-button"
                 color="primary"
@@ -19,6 +27,7 @@
               >
                 Log In with Business BCeID<v-icon>mdi-login</v-icon>
               </v-btn>  
+
             </v-col>
           </v-row>
         </v-container>  
@@ -29,9 +38,9 @@
         <v-container class="justify-center">
           <v-row no-gutters>
             <v-col>
-              <p class="text-welcome centered-text mt-14 mb-8">
+              <h3 class="centered-text mt-14 mb-8">
                 How to use the tool
-              </p>
+              </h3>
             </v-col>
           </v-row>
           <v-row class="mb-4 d-flex justify-center w-100">
@@ -39,13 +48,13 @@
 
               <v-row>
                 <v-col class="d-flex justify-center align-center">                                    
-                  <div class="circle" :class="{ active: stage == 'UPLOAD', disabled: stage != 'UPLOAD'  }" v-on:click="stage = 'UPLOAD'">1</div>
+                  <div class="circle" :class="{ active: stage == 'UPLOAD', disabled: stage != 'UPLOAD'  }" v-on:click="changeStage('UPLOAD')">1</div>
                 </v-col>
                 <v-col class="d-flex justify-center align-center">
-                  <div class="circle" :class="{ active: stage == 'REVIEW', disabled: stage != 'REVIEW' }" v-on:click="stage = 'REVIEW'">2</div>
+                  <div class="circle" :class="{ active: stage == 'REVIEW', disabled: stage != 'REVIEW' }" v-on:click="changeStage('REVIEW')">2</div>
                 </v-col>
                 <v-col class="d-flex justify-center align-center">
-                  <div class="circle" :class="{ active: stage == 'GENERATE', disabled: stage != 'GENERATE' }" v-on:click="stage = 'GENERATE'">3</div>
+                  <div class="circle" :class="{ active: stage == 'GENERATE', disabled: stage != 'GENERATE' }" v-on:click="changeStage('GENERATE')">3</div>
                 </v-col>
               </v-row>
 
@@ -85,31 +94,39 @@
                 </v-col>
               </v-row>
 
-              <v-row class="mt-16 mb-8">
-                <v-col class="d-flex justify-center no-padding">
+              <v-row class="mt-8 mb-2">
+                <v-col class="d-flex justify-center pt-10">
                   
                   <div v-show="stage == 'UPLOAD'">
                     <h3 class="mb-4">Upload your company's data</h3>
-                    <p class="mb-8">Introducing the Pay Transparency Reporting tool, onsectetur adipiscing elit. Ut fermentum tortor ut ultricies placerat. Nam eu leo lacus. Quisque id tempor arcu. Ut fermentum tortor ut ultricies placer, visit <u>Pay Transparency Laws in B.C.</u></p>
+                    <p class="mb-8">
+                      Please review the <a target="_blank" href="https://www2.gov.bc.ca/assets/gov/british-columbians-our-governments/services-policies-for-government/gender-equity/guidance-for-pay-gap-reporting-nov-03-2023.pdf">Guidance for Preparing Pay Transparency Reports</a> to ensure you understand what must be in your report. You will also be able to include general information about your organization and any constraints or limitations with your data (i.e. no overtime hours permitted).  When you are ready, upload a file of your employee data in comma-separated value (CSV) format.  
+                    </p>
                     <v-btn>Download sample CSV</v-btn>
                   </div>
                   <p v-show="stage == 'REVIEW'">
                     <h3 class="mb-4">Review and validate</h3>
-                    Introducing the Pay Transparency Reporting tool, onsectetur adipiscing elit. Ut fermentum tortor ut ultricies placerat. Nam eu leo lacus. Quisque id tempor arcu. Ut fermentum tortor ut ultricies placer, visit <u>Pay Transparency Laws in B.C.</u>
+                    <p class="mb-8">
+                      Please carefully review any error messages and confirm the information you provided is accurate before moving forward.  The <a target="_blank" href="https://www2.gov.bc.ca/assets/gov/british-columbians-our-governments/services-policies-for-government/gender-equity/guidance-for-pay-gap-reporting-nov-03-2023.pdf">Guidance for Preparing Pay Transparency Reports</a> details what must be included in your report.
+                    </P>
                   </p>
                   <p v-show="stage == 'GENERATE'">
                     <h3 class="mb-4">Generate your report</h3>
-                    <p class="mb-8">Introducing the Pay Transparency Reporting tool, onsectetur adipiscing elit. Ut fermentum tortor ut ultricies placerat. Nam eu leo lacus. Quisque id tempor arcu. Ut fermentum tortor ut ultricies placer, visit <u>Pay Transparency Laws in B.C.</u></p>
+                    <p class="mb-8">
+                      You will now be able to download and review a draft version of your report. Draft reports are not saved in the system. Once your report is finalized, it will be available in the system in your employer profile for retrieval and distribution.
+                    </p>
                     <v-btn>View sample report</v-btn>
                   </p>                                    
                 </v-col>
                 <v-col class="d-flex justify-center aligned-right no-padding">
                   <img
-                      width="400px"
+                      class="image-monitor"
+                      width="562px"
+                      height="370px"
                       tabindex="-1"
-                      src="../assets/images/upload.png"
+                      :src="imageSource"
                       alt="Monitor"
-                  >                  
+                  >  
                 </v-col>
               </v-row>               
 
@@ -128,6 +145,10 @@ import { mapState } from 'pinia';
 import { AuthRoutes } from '../utils/constant';
 import { sanitizeUrl } from '@braintree/sanitize-url';
 
+import uploadpng from "../assets/images/upload_screen.png"
+import generatepng from "../assets/images/generate_screen.png"
+import reviewpng from "../assets/images/review_screen.png"
+
 export default {
   name: 'Login',
   components: {
@@ -137,7 +158,8 @@ export default {
     return {
       appTitle: 'Pay Transparency Reporting',
       authRoutesLogin: sanitizeUrl(AuthRoutes.LOGIN_BCEID),
-      stage: "UPLOAD"
+      stage: "UPLOAD",
+      imageSource: uploadpng
     };
   },
   computed: {
@@ -147,12 +169,33 @@ export default {
     clearStorageAndRedirectToLogin() {
       authStore().setJwtToken();
       window.location.href = this.authRoutesLogin;
+    },
+    changeStage: function(newstage) {
+      switch(newstage) {
+        case 'GENERATE':
+          this.stage = 'GENERATE';
+          this.imageSource = generatepng;
+          break;
+        case 'REVIEW':
+          this.stage = 'REVIEW';
+          this.imageSource = reviewpng;
+          break;
+        case 'UPLOAD':
+          this.stage = 'UPLOAD';
+          this.imageSource = uploadpng;
+          break;
+      }
     }
   }
 };
 </script>
 
 <style scoped>
+  .image-monitor {
+    border-style: none;
+    margin-left: auto;   
+  }
+
   .full-height{
     height: 100%;
   }
@@ -201,12 +244,12 @@ export default {
   }  
 
   .text-welcome {
-    font-size: 1.4em;
+    font-size: 1.6em;
     margin-block-start: 1em;
     margin-block-end: 1em;
     margin-inline-start: 0px;
     margin-inline-end: 0px;
-    font-weight: 500;
+    font-weight: 600;
   }
 
   .grey-div {
@@ -214,7 +257,7 @@ export default {
     height: 100%;
   }
 
-  .unbastardized {
+  .undized {
     margin: 0;
     padding: 0;
     min-width: 100%;
