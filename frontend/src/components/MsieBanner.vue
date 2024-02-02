724ec22fa9e79789bc7b937fb9d4f6336a2cc3da
jer3k <99355997+jer3k@users.noreply.github.com>
2024-02-02 15:31:02 -0800

    prettier

--- frontend/src/components/MsieBanner.vue
+++ frontend/src/components/MsieBanner.vue
@@ -1,16 +1,12 @@
 <template>
-  <v-alert
-    id="banner"
-    dense
-    type="error"
-    dismissible
-  >
-    Your current web browser, Microsoft Internet Explorer, is not supported for this service. Use one of these web browsers: Microsoft Edge, Mozilla Firefox, Google Chrome, or Apple Safari.
+  <v-alert id="banner" dense type="error" dismissible>
+    Your current web browser, Microsoft Internet Explorer, is not supported for
+    this service. Use one of these web browsers: Microsoft Edge, Mozilla
+    Firefox, Google Chrome, or Apple Safari.
   </v-alert>
 </template>
 
 <style scoped>
-
 #banner {
   color: #81692c;
   background-color: #fef4dd !important;
@@ -32,5 +28,4 @@
   margin: 0 auto;
   max-width: 100vw;
 }
-
 </style>
