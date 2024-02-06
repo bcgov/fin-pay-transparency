3da282c106e0cb7111d600bc2e085209b68fe866
goemen <gnthomiwa@gmail.com>
2024-02-06 09:30:21 -0800

    adding a null check

--- backend/src/v1/routes/auth-routes.ts
+++ backend/src/v1/routes/auth-routes.ts
@@ -118,7 +118,7 @@ router.post(
     const session: any = req.session;
     const errors = validationResult(req);
 
-    if (!user.companyDetails) {
+    if (!user?.companyDetails) {
       return res.status(401).json({error: 'Missing company details'});
     }
 
@@ -151,7 +151,7 @@ router.get(
   utils.asyncHandler(auth.refreshJWT),
   (req: Request, res: Response) => {
     const user: any = req.user;
-    if (!user.companyDetails) {
+    if (!user?.companyDetails) {
       return res.status(401).json({ error: 'Missing company details' });
     }
     const session: any = req.session;
