3c866a6489960d189eaf37046fbb66281ceae1e6
Brock Anderson <brock@bandersgeo.ca>
2024-02-05 16:34:50 -0800

    improved error handling

--- backend/src/v1/services/report-service.ts
+++ backend/src/v1/services/report-service.ts
@@ -354,12 +354,13 @@ const reportServicePrivate = {
 const reportService = {
   /*
   Fetches a report identified by the given reportId from the database,
-  along with the calculated data associated with that report
+  along with the calculated data associated with that report.
+  If no report with the given id is found, returns null.
   */
   async getReportAndCalculations(
     req,
     reportId: string,
-  ): Promise<ReportAndCalculations> {
+  ): Promise<ReportAndCalculations | null> {
     let reportAndCalculations: ReportAndCalculations | null = null;
     const userInfo = utils.getSessionUser(req);
     if (!userInfo) {
@@ -375,20 +376,25 @@ const reportService = {
           },
         });
 
-      const report = await tx.pay_transparency_report.findFirst({
-        where: {
-          company_id: payTransparencyCompany.company_id,
-          report_id: reportId,
-        },
-        include: {
-          pay_transparency_company: true,
-          naics_code_pay_transparency_report_naics_codeTonaics_code: true,
-          employee_count_range: true,
-        },
-      });
+      let report = null;
+      try {
+        report = await tx.pay_transparency_report.findFirst({
+          where: {
+            company_id: payTransparencyCompany.company_id,
+            report_id: reportId,
+          },
+          include: {
+            pay_transparency_company: true,
+            naics_code_pay_transparency_report_naics_codeTonaics_code: true,
+            employee_count_range: true,
+          },
+        });
+      } catch (e) {
+        console.log(e);
+      }
 
       if (!report) {
-        throw new Error('Not found');
+        return null;
       }
 
       const calculatedDatas =
@@ -466,14 +472,19 @@ const reportService = {
     return explanatoryNotes;
   },
 
-  async getReportData(req, reportId: string): Promise<object> {
+  async getReportData(req, reportId: string): Promise<object | null> {
     logger.debug(
-      `getReportHtml called with reportId: ${reportId} and correlationId: ${req.session?.correlationID}`,
+      `getReportData called with reportId: ${reportId} and correlationId: ${req.session?.correlationID}`,
     );
     const reportAndCalculations = await this.getReportAndCalculations(
       req,
       reportId,
     );
+
+    if (!reportAndCalculations) {
+      return null;
+    }
+
     const report = reportAndCalculations.report;
     const calcs = reportAndCalculations.calculations;
     const referenceGenderCode: string =
@@ -893,9 +904,9 @@ const reportService = {
       );
     if (!referenceGenderChartInfo) {
       throw new Error(
-        `Cannot find chart info for the reference category '${calcs[
-          CALCULATION_CODES.REFERENCE_GENDER_CATEGORY_CODE
-        ]?.value}'`,
+        `Cannot find chart info for the reference category '${
+          calcs[CALCULATION_CODES.REFERENCE_GENDER_CATEGORY_CODE]?.value
+        }'`,
       );
     }
 
