8862aef9839a3a33a673c3902ab6abf124bc95c3
Brock Anderson <brock@bandersgeo.ca>
2024-02-07 17:22:02 -0800

    renamed

--- backend/db/migrations/V1.0.14_report_history_fix_data_constraints.sql
+++
@@ -1,5 +0,0 @@
-SET search_path TO pay_transparency;
-
--- The previous migration set the incorrect length of the 
--- report_history.data_constraints column.  Fix that here.
-alter table report_history alter column data_constraints TYPE varchar(3000);
