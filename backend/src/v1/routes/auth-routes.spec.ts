f694f07356dbf51d89ca36ff050b3aaad5fbea24
goemen <gnthomiwa@gmail.com>
2024-02-08 13:13:17 -0800

    adding more tests

--- backend/src/v1/routes/auth-routes.spec.ts
+++ backend/src/v1/routes/auth-routes.spec.ts
@@ -1,16 +1,26 @@
 import express, { Application } from 'express';
 import router from './auth-routes';
 import request from 'supertest';
+import { MISSING_COMPANY_DETAILS_ERROR } from '../../constants';
 let app: Application;
 
 const mockHandleCallbackBusinessBceid = jest.fn();
+const mockIsTokenExpired = jest.fn();
+const mockIsRenewable = jest.fn();
+const mockRenew = jest.fn();
+const mockGenerateUiToken = jest.fn();
 jest.mock('../services/auth-service', () => ({
   auth: {
     handleCallBackBusinessBceid: (...args) =>
       mockHandleCallbackBusinessBceid(...args),
-    refreshJWT: () => jest.fn(() => mockRefreshJWT()),
+    refreshJWT: jest.fn((req, res, next) => mockRefreshJWT(req, res, next)),
+    isTokenExpired: () => mockIsTokenExpired(),
+    isRenewable: () => mockIsRenewable(),
+    renew: () => mockRenew(),
+    generateUiToken: () => mockGenerateUiToken(),
   },
 }));
+
 const mockGetOidcDiscovery = jest.fn();
 jest.mock('../services/utils-service', () => ({
   ...jest.requireActual('../services/utils-service'),
@@ -25,6 +35,16 @@ const mockRefreshJWT = jest.fn();
 jest.mock('passport', () => ({
   authenticate: (...args) =>
     jest.fn((req, res, next) => mockAuthenticate(req, res, next)),
+}));
+
+const mockExists = jest.fn();
+const mockValidationResult = jest.fn();
+jest.mock('express-validator', () => ({
+  ...jest.requireActual('express-validator'),
+  body: (...args) => ({
+    exists: () => jest.fn((req, res, next) => mockExists(req, res, next)),
+  }),
+  validationResult: jest.fn(() => mockValidationResult()),
 }));
 
 const mockLogout = jest.fn();
@@ -176,6 +196,273 @@ describe('auth-routes', () => {
         .expect(400)
         .then(() => {
           expect(errorHandler).toHaveBeenCalled();
+        });
+    });
+  });
+
+  describe('/refresh', () => {
+    it('return 401 if token company details are missing', () => {
+      mockExists.mockImplementation((req, res, next) => next());
+      app.use((req: any, res, next) => {
+        req.session = mockRequest.session;
+        req.user = mockRequest.user;
+        req.logout = mockRequest.logout;
+        next();
+      });
+      app.use('/auth', router);
+
+      return request(app)
+        .post('/auth/refresh')
+        .expect(401)
+        .expect((res) => {
+          expect(res.body.error).toBe(MISSING_COMPANY_DETAILS_ERROR);
+        });
+    });
+    it('should handle validation errors', () => {
+      mockExists.mockImplementation((req, res, next) => next());
+      mockValidationResult.mockImplementation(() => {
+        return {
+          isEmpty: () => false,
+          array: () => [1, 2],
+        };
+      });
+      app.use((req: any, res, next) => {
+        req.session = { ...mockRequest.session, companyDetails: { id: 1 } };
+        req.user = mockRequest.user;
+        req.logout = mockRequest.logout;
+        next();
+      });
+      app.use('/auth', router);
+
+      return request(app)
+        .post('/auth/refresh')
+        .expect(400)
+        .expect((res) => {
+          expect(res.body.errors).toEqual([1, 2]);
+        });
+    });
+    it('should return unauthorized when jwt or refresh token is not found', () => {
+      mockExists.mockImplementation((req, res, next) => next());
+      mockValidationResult.mockImplementation(() => {
+        return {
+          isEmpty: () => true,
+        };
+      });
+      app.use((req: any, res, next) => {
+        req.session = { ...mockRequest.session, companyDetails: { id: 1 } };
+        req.user = mockRequest.user;
+        req.logout = mockRequest.logout;
+        next();
+      });
+      app.use('/auth', router);
+
+      return request(app)
+        .post('/auth/refresh')
+        .expect(401)
+        .expect((res) => {
+          expect(res.body).toEqual({
+            error: 'Unauthorized',
+            error_description: 'Not logged in',
+          });
+        });
+    });
+    it('should renew the token if renewable', () => {
+      mockExists.mockImplementation((req, res, next) => next());
+      mockValidationResult.mockImplementation(() => {
+        return {
+          isEmpty: () => true,
+        };
+      });
+      mockIsTokenExpired.mockReturnValue(true);
+      mockGenerateUiToken.mockReturnValue('jwt_value');
+      mockIsRenewable.mockReturnValue(true);
+      mockRenew.mockResolvedValue({ jwt: 1, refreshToken: 1 });
+
+      app.use((req: any, res, next) => {
+        req.session = {
+          ...mockRequest.session,
+          companyDetails: { id: 1 },
+          correlationID: 12,
+        };
+        req.user = { ...mockRequest.user, jwt: 'jwt', refreshToken: 'jwt' };
+        req.logout = mockRequest.logout;
+        next();
+      });
+      app.use('/auth', router);
+
+      return request(app)
+        .post('/auth/refresh')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toEqual({
+            jwtFrontend: 'jwt_value',
+            correlationID: 12,
+          });
+        });
+    });
+    it('should return unauthorized when it fails to renew tokens', () => {
+      mockExists.mockImplementation((req, res, next) => next());
+      mockValidationResult.mockImplementation(() => {
+        return {
+          isEmpty: () => true,
+        };
+      });
+      mockIsTokenExpired.mockReturnValue(true);
+      mockGenerateUiToken.mockReturnValue('jwt_value');
+      mockIsRenewable.mockReturnValue(true);
+      mockRenew.mockResolvedValue({});
+
+      app.use((req: any, res, next) => {
+        req.session = {
+          ...mockRequest.session,
+          companyDetails: { id: 1 },
+          correlationID: 12,
+        };
+        req.user = { ...mockRequest.user, jwt: 'jwt', refreshToken: 'jwt' };
+        req.logout = mockRequest.logout;
+        next();
+      });
+      app.use('/auth', router);
+
+      return request(app).post('/auth/refresh').expect(401);
+    });
+    it('should return unauthorized if token is not expired', () => {
+      mockExists.mockImplementation((req, res, next) => next());
+      mockValidationResult.mockImplementation(() => {
+        return {
+          isEmpty: () => true,
+        };
+      });
+      mockIsTokenExpired.mockReturnValue(true);
+      mockIsRenewable.mockReturnValue(false);
+
+      app.use((req: any, res, next) => {
+        req.session = {
+          ...mockRequest.session,
+          companyDetails: { id: 1 },
+          correlationID: 12,
+        };
+        req.user = { ...mockRequest.user, jwt: 'jwt', refreshToken: 'jwt' };
+        req.logout = mockRequest.logout;
+        next();
+      });
+      app.use('/auth', router);
+
+      return request(app).post('/auth/refresh').expect(401);
+    });
+    it('should return with current user tokens', () => {
+      mockExists.mockImplementation((req, res, next) => next());
+      mockValidationResult.mockImplementation(() => {
+        return {
+          isEmpty: () => true,
+        };
+      });
+      mockIsTokenExpired.mockReturnValue(false);
+
+      app.use((req: any, res, next) => {
+        req.session = {
+          ...mockRequest.session,
+          companyDetails: { id: 1 },
+          correlationID: 12,
+        };
+        req.user = {
+          ...mockRequest.user,
+          jwtFrontend: 'jwt',
+          jwt: 'jwt',
+          refreshToken: 'jwt',
+        };
+        req.logout = mockRequest.logout;
+        next();
+      });
+      app.use('/auth', router);
+
+      return request(app)
+        .post('/auth/refresh')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toEqual({
+            jwtFrontend: 'jwt',
+            correlationID: 12,
+          });
+        });
+    });
+  });
+  describe('/token', () => {
+    it('return 401 if token company details are missing', () => {
+      mockRefreshJWT.mockImplementation((req, res, next) => next());
+      mockExists.mockImplementation((req, res, next) => next());
+      app.use((req: any, res, next) => {
+        req.session = mockRequest.session;
+        req.user = mockRequest.user;
+        req.logout = mockRequest.logout;
+        next();
+      });
+      app.use('/auth', router);
+
+      return request(app)
+        .get('/auth/token')
+        .expect(401)
+        .expect((res) => {
+          expect(res.body.error).toBe(MISSING_COMPANY_DETAILS_ERROR);
+        });
+    });
+    it('return 200 if jwtFrontend and refreshToken are available', () => {
+      mockRefreshJWT.mockImplementation((req, res, next) => next());
+      mockExists.mockImplementation((req, res, next) => next());
+      app.use((req: any, res, next) => {
+        req.session = {
+          ...mockRequest.session,
+          correlationID: 1,
+          companyDetails: {},
+          passport: {
+            user: {
+              _json: {},
+            },
+          },
+        };
+        req.user = {
+          ...mockRequest.user,
+          jwtFrontend: 'jwtFrontend',
+          refreshToken: 'refreshToken',
+        };
+        req.logout = mockRequest.logout;
+        next();
+      });
+      app.use('/auth', router);
+
+      return request(app)
+        .get('/auth/token')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.jwtFrontend).toBeDefined();
+          expect(res.body.correlationID).toBeDefined();
+        });
+    });
+    it('return 401 unauthorized', () => {
+      mockRefreshJWT.mockImplementation((req, res, next) => next());
+      mockExists.mockImplementation((req, res, next) => next());
+      app.use((req: any, res, next) => {
+        req.session = {
+          ...mockRequest.session,
+          correlationID: 1,
+          companyDetails: {},
+        };
+        req.user = {
+          ...mockRequest.user,
+        };
+        req.logout = mockRequest.logout;
+        next();
+      });
+      app.use('/auth', router);
+
+      return request(app)
+        .get('/auth/token')
+        .expect(401)
+        .expect((res) => {
+          expect(res.body).toEqual({
+            error: 'Unauthorized',
+            error_description: 'Not logged in',
+          });
         });
     });
   });
