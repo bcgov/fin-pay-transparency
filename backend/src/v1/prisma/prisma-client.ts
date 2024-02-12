d95ee65f5a5887c2be9567241b7019a796672f3b
jer3k <99355997+jer3k@users.noreply.github.com>
2024-02-12 12:27:21 -0800

    fix exporting non-const variable

--- backend/src/v1/prisma/prisma-client.ts
+++ backend/src/v1/prisma/prisma-client.ts
@@ -1,7 +1,5 @@
 import { logger } from '../../logger';
-
-let prisma: PrismaClient;
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const DB_HOST = process.env.POSTGRESQL_HOST || 'localhost';
 const DB_USER = process.env.POSTGRESQL_USER || 'postgres';
@@ -11,26 +9,26 @@ const DB_PWD = encodeURIComponent(
 const DB_PORT = process.env.POSTGRESQL_PORT || 5432;
 const DB_NAME = process.env.POSTGRESQL_DATABASE || 'postgres';
 const DB_SCHEMA = process.env.DB_SCHEMA || 'pay_transparency';
-if (!prisma) {
-  const datasourceUrl = `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`;
-  logger.silly(`Connecting to ${datasourceUrl}`);
-  prisma = new PrismaClient({
-    log: [
-      { emit: 'event', level: 'query' },
-      { emit: 'stdout', level: 'info' },
-      { emit: 'stdout', level: 'warn' },
-      { emit: 'stdout', level: 'error' },
-    ],
-    errorFormat: 'pretty',
-    datasourceUrl: datasourceUrl,
-  });
-  // @ts-expect-error, this is a prisma thing.
-  prisma.$on('query', (e) => {
-    // @ts-expect-error, this is a prisma thing.
-    logger.debug(
-      `Query: ${e.query}- Params: ${e.params} - Duration: ${e.duration}ms`,
-    );
-  });
-}
+
+const datasourceUrl = `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`;
+logger.silly(`Connecting to ${datasourceUrl}`);
+const prisma: PrismaClient<
+  Prisma.PrismaClientOptions,
+  'query' | 'info' | 'warn' | 'error'
+> = new PrismaClient({
+  log: [
+    { emit: 'event', level: 'query' },
+    { emit: 'stdout', level: 'info' },
+    { emit: 'stdout', level: 'warn' },
+    { emit: 'stdout', level: 'error' },
+  ],
+  errorFormat: 'pretty',
+  datasourceUrl: datasourceUrl,
+});
+prisma.$on('query', (e) => {
+  logger.debug(
+    `Query: ${e.query}- Params: ${e.params} - Duration: ${e.duration}ms`,
+  );
+});
 
 export default prisma;
