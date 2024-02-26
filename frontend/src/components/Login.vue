c216615c17e8e69df68cbc9b3a954454e46d10a0
andybcgov <118761047+andybcgov@users.noreply.github.com>
2024-02-26 09:10:59 -0800

    change-word-on-the-landing-page

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -17,7 +17,7 @@
                   <li>2026: all employers with 50 employees or more</li>
                 </ul>
               </p>
-              <p class="text-subtitle-2 mb-2">This portal enables you to easily create a pay transparency report as required under the <a target="_blank" rel="noopener noreferrer" href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/23018">Pay Transparency Act</a> and <a target="_blank" rel="noopener noreferrer" href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/225_2023">Regulation</a>.</p>
+              <p class="text-subtitle-2 mb-2">This tool enables you to easily create a pay transparency report as required under the <a target="_blank" rel="noopener noreferrer" href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/23018">Pay Transparency Act</a> and <a target="_blank" rel="noopener noreferrer" href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/225_2023">Regulation</a>.</p>
               <p class="text-subtitle-2 mb-4">Below is an overview of the three steps required to produce your pay transparency report. For additional information please visit <a target="_blank" rel="noopener noreferrer" href="https://www2.gov.bc.ca/gov/content/gender-equity/pay-transparency-laws-in-bc">Pay Transparency Laws in B.C.</a></p>
               <v-btn
                 id="login-button"
