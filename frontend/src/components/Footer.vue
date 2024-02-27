5d5237c5acaacad534152f86887a56c0c9f04f2e
jer3k <99355997+jer3k@users.noreply.github.com>
2024-02-26 16:27:51 -0800

    underline and seperate footler links

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer color="#003366" dark absolute>
     <v-row no-gutters>
-      <v-col class="justify-center">
+      <v-col class="justify-center" style="text-align: center">
         For questions or assistance with creating a report please contact the
         Pay Transparency Unit -
         <a class="contact-email" href="mailto:paytransparency@gov.bc.ca">
@@ -15,55 +15,68 @@
       <v-col class="justify-center">
         <v-btn
           id="footer-home"
-          variant="text"
+          class="footer-btn pl-1 pr-1"
+          variant="plain"
           color="white"
           href="https://www.gov.bc.ca/"
         >
           Home
         </v-btn>
+        |
         <v-btn
           id="footer-about"
-          variant="text"
+          class="footer-btn pl-1 pr-1"
+          variant="plain"
           color="white"
           href="https://www2.gov.bc.ca/gov/content/about-gov-bc-ca"
         >
           About gov.bc.ca
         </v-btn>
+        |
         <v-btn
           id="footer-disclaimer"
-          variant="text"
+          class="footer-btn pl-1 pr-1"
+          variant="plain"
           color="white"
           href="http://gov.bc.ca/disclaimer"
         >
           Disclaimer
         </v-btn>
+        |
         <v-btn
           id="footer-privacy"
-          variant="text"
+          class="footer-btn pl-1 pr-1"
+          variant="plain"
           color="white"
           href="http://gov.bc.ca/privacy"
         >
           Privacy
         </v-btn>
+        |
         <v-btn
           id="footer-accessibility"
-          variant="text"
+          class="footer-btn pl-1 pr-1"
+          variant="plain"
           color="white"
           href="http://gov.bc.ca/webaccessibility"
         >
           Accessibility
         </v-btn>
+        |
         <v-btn
           id="footer-copyright"
-          variant="text"
+          class="footer-btn pl-1 pr-1"
+          variant="plain"
           color="white"
           href="http://gov.bc.ca/copyright"
         >
           Copyright
         </v-btn>
+        |
         <v-btn
           id="footer-contact"
-          variant="text"
+          class="footer-btn pl-1 pr-1"
+          variant="plain"
           color="white"
           href="https://www2.gov.bc.ca/gov/content/home/contact-us"
         >
@@ -74,22 +87,19 @@
   </v-footer>
 </template>
 
-<script>
-export default {};
-</script>
+<script setup lang="ts"></script>
 
 <style>
 .v-footer {
   flex-direction: column;
   min-width: 100%;
-  max-height: 2.5em;
 }
 
 .v-footer.bordered {
   border-top: 2px solid rgb(252, 186, 25) !important;
+  overflow: hidden;
   max-height: 3em;
 }
-
 a.contact-email {
   color: white;
   text-decoration: underline;
@@ -99,5 +109,8 @@ a.contact-email {
 .v-btn.footer:focus:before,
 .v-btn.footer:hover:before {
   background: none;
+}
+.footer-btn:hover {
+  text-decoration: underline !important;
 }
 </style>
