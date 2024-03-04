5ede39fce8315aec94ab387e8d74ab824a241eb6
Goeme Nthomiwa <gnthomiwa@gmail.com>
2024-03-04 09:43:56 -0800

    Feature/add report edit locking mechanism (#279)

--- frontend/src/common/types/index.ts
+++ frontend/src/common/types/index.ts
@@ -1,3 +1,12 @@
 export interface IConfigValue {
   maxUploadFileSize: number;
+  reportEditDurationInDays: number;
+}
+
+export interface IReport {
+  report_id: string;
+  report_start_date: string;
+  report_end_date: string;
+  create_date: string;
+  is_unlocked: boolean;
 }
