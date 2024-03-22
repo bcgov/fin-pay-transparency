b1acf28296ee886a754eb8fd7b9aec350e6ede1c
Sukanya Rath <98050194+sukanya-rath@users.noreply.github.com>
2024-03-21 17:18:47 -0700

    still trying

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
+    ['line'],
     ['list', { printSteps: true }],
     ['html', {open: 'always'}]
   ],
