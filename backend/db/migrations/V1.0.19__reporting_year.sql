1f4d45feabf80d1e8532ad8531e4d05763f01f68
jer3k <99355997+jer3k@users.noreply.github.com>
2024-03-26 14:07:05 -0700

    fix potential issue with test environment

--- backend/db/migrations/V1.0.19__reporting_year.sql
+++ backend/db/migrations/V1.0.19__reporting_year.sql
@@ -1,6 +1,6 @@
 SET search_path TO pay_transparency;
 
-alter table pay_transparency_report add column reporting_year numeric not null default 2023;
+alter table pay_transparency_report add column reporting_year numeric not null default 2024;
 alter table pay_transparency_report alter column reporting_year drop default;
-alter table report_history add column reporting_year numeric not null default 2023;
+alter table report_history add column reporting_year numeric not null default 2024;
 alter table report_history alter column reporting_year drop default;
