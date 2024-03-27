18c65dec9a71f9a6492a485d0109785778be933c
Brock Anderson <brock@bandersgeo.ca>
2024-03-26 17:06:34 -0700

    fixed unit tests

--- frontend/src/components/util/__tests__/SnackBar.spec.ts
+++ frontend/src/components/util/__tests__/SnackBar.spec.ts
@@ -1,82 +1,81 @@
 import { mount } from '@vue/test-utils';
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
-import { createVuetify } from "vuetify";
+import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
-import { Notification, NotificationTypes } from '../../../common/notificationService';
+import {
+  INotification,
+  NotificationSeverity,
+} from '../../../common/notificationService';
 import SnackBar from '../SnackBar.vue';
 
-describe("SnackBar", () => {
+describe('SnackBar', () => {
   let wrapper;
 
   beforeEach(() => {
-
-    //create an instance of vuetify so we can inject it 
+    //create an instance of vuetify so we can inject it
     //into the mounted component, allowing it to behave as it would
     //in a browser
     const vuetify = createVuetify({
       components,
       directives,
-    })
+    });
 
     wrapper = mount(SnackBar, {
       global: {
         plugins: [vuetify],
-      }
-    })
-
-  })
+      },
+    });
+  });
 
   afterEach(() => {
     if (wrapper) {
       wrapper.unmount();
     }
-  })
+  });
 
-  describe("when one notification event is received", () => {
-    it("the snackbar becomes visible and displays the notification", async () => {
-      const notification: Notification = {
-        message: "fake error message",
-        severity: NotificationTypes.NOTIFICATION_ERROR
+  describe('when one notification event is received', () => {
+    it('the snackbar becomes visible and displays the notification', async () => {
+      const notification: INotification = {
+        message: 'fake error message',
+        severity: NotificationSeverity.ERROR,
+        timeoutMs: 5000,
       };
 
-      expect(wrapper.vm.isVisible).toBeFalsy()
+      expect(wrapper.vm.isVisible).toBeFalsy();
 
       wrapper.vm.onNotificationEvent(notification);
 
       // Check that data values in the component were correctly modified
-      expect(wrapper.vm.message).toBe(notification.message);
+      expect(wrapper.vm.activeNotification).toStrictEqual(notification);
       expect(wrapper.vm.isVisible).toBeTruthy();
+    });
+  });
 
-
-    })
-  })
-
-  describe("when close() is called", () => {
-    it("the snackbar becomes hidden", async () => {
-
+  describe('when close() is called', () => {
+    it('the snackbar becomes hidden', async () => {
       wrapper.vm.isVisible = true;
-      wrapper.vm.message = "a message";
+      wrapper.vm.message = 'a message';
 
       wrapper.vm.close();
 
       // Check that data values in the component were correctly modified
-      expect(wrapper.vm.message).toBe(null);
+      expect(wrapper.vm.activeNotification).toBe(null);
       expect(wrapper.vm.isVisible).toBeFalsy();
+    });
+  });
 
-    })
-  })
-
-  describe("when processNextNotification(..) is called with another notification in the queue", () => {
-    it("the current notification is replaced with the next one", async () => {
-
-      const notification1: Notification = {
-        message: "notification 1",
-        severity: NotificationTypes.NOTIFICATION_ERROR
+  describe('when processNextNotification(..) is called with another notification in the queue', () => {
+    it('the current notification is replaced with the next one', async () => {
+      const notification1: INotification = {
+        message: 'notification 1',
+        severity: NotificationSeverity.ERROR,
+        timeoutMs: 5000,
       };
-      const notification2: Notification = {
-        message: "notification 2",
-        severity: NotificationTypes.NOTIFICATION_INFO
+      const notification2: INotification = {
+        message: 'notification 2',
+        severity: NotificationSeverity.INFO,
+        timeoutMs: 5000,
       };
 
       // Simulate receiving two notifications in quick succession
@@ -84,26 +83,24 @@ describe("SnackBar", () => {
       wrapper.vm.onNotificationEvent(notification2);
 
       // Check that the first notification is displayed
-      expect(wrapper.vm.message).toBe(notification1.message);
+      expect(wrapper.vm.activeNotification).toStrictEqual(notification1);
       expect(wrapper.vm.isVisible).toBeTruthy();
 
       // Simulate the first notification expiring
-      // (normally this happens after a few seconds, 
-      // but we trigger it immediately here)      
+      // (normally this happens after a few seconds,
+      // but we trigger it immediately here)
       wrapper.vm.onNotificationExpired();
 
       // Check that the first notification is displayed
-      expect(wrapper.vm.message).toBe(notification2.message);
+      expect(wrapper.vm.activeNotification).toStrictEqual(notification2);
       expect(wrapper.vm.isVisible).toBeTruthy();
 
       // Cause the second notification to expire
       wrapper.vm.onNotificationExpired();
 
       // Confirm that no more notifications are displayed
-      expect(wrapper.vm.message).toBe(null);
+      expect(wrapper.vm.activeNotification).toBe(null);
       expect(wrapper.vm.isVisible).toBeFalsy();
-
-    })
-  })
-
-})
+    });
+  });
+});
