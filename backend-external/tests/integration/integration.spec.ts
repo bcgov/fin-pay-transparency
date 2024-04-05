1e5c19228f42d173a77e590d9fd3e1c9201bef77
jer3k <99355997+jer3k@users.noreply.github.com>
2024-04-05 14:44:35 -0700

    remove some tests

--- backend-external/tests/integration/integration.spec.ts
+++ backend-external/tests/integration/integration.spec.ts
@@ -5,41 +5,29 @@ const request = agent(config.get('server:baseURL'));
 
 describe('/ GET', () => {
   it('the service should be running', () => {
-    return request.get('').expect(200);
-  });
-});
-
-describe('/health/ GET', () => {
-  it('the service should be running', () => {
-    return request.get('/health').expect(200);
-  });
-});
-
-describe('/prom-metrics/ GET', () => {
-  it('the service should be running', () => {
-    return request.get('/prom-metrics').expect(200);
+    request.get('').expect(200);
   });
 });
 
 describe('/v1/docs/ GET', () => {
   it('the service should be running', () => {
-    return request.get('/v1/docs/').expect(200);
+    request.get('/v1/docs/').expect(200);
   });
 });
 
 describe('/v1/pay-transparency/ GET', () => {
   it('returns error if secret key is not set', () => {
-    return request.get('/v1/pay-transparency').expect(400);
+    request.get('/v1/pay-transparency').expect(400);
   });
   it('returns error if secret key is wrong', () => {
-    return request
+    request
       .get('/v1/pay-transparency')
       .set('x-api-key', 'wrong_key')
       .expect(401);
   });
   it('returns data if secret key is provided', () => {
     //note: this test requires both backend-external and backend to be running.
-    return request
+    request
       .get('/v1/pay-transparency')
       .set('x-api-key', config.get('server:apiKey'))
       .expect(200)
