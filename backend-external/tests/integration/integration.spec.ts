00d1bef2e7649da21046da94f5cdd7ffe0679a3b
jer3k <99355997+jer3k@users.noreply.github.com>
2024-04-08 11:22:08 -0700

    fix returning promisses

--- backend-external/tests/integration/integration.spec.ts
+++ backend-external/tests/integration/integration.spec.ts
@@ -5,29 +5,29 @@ const request = agent(config.get('server:baseURL'));
 
 describe('/ GET', () => {
   it('the service should be running', () => {
-    request.get('').expect(200);
+    return request.get('').expect(200);
   });
 });
 
 describe('/v1/docs/ GET', () => {
   it('the service should be running', () => {
-    request.get('/v1/docs/').expect(200);
+    return request.get('/v1/docs/').expect(200);
   });
 });
 
 describe('/v1/pay-transparency/ GET', () => {
   it('returns error if secret key is not set', () => {
-    request.get('/v1/pay-transparency').expect(400);
+    return request.get('/v1/pay-transparency').expect(400);
   });
   it('returns error if secret key is wrong', () => {
-    request
+    return request
       .get('/v1/pay-transparency')
       .set('x-api-key', 'wrong_key')
       .expect(401);
   });
   it('returns data if secret key is provided', () => {
     //note: this test requires both backend-external and backend to be running.
-    request
+    return request
       .get('/v1/pay-transparency')
       .set('x-api-key', config.get('server:apiKey'))
       .expect(200)
