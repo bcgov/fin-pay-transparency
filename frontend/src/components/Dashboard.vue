bb18b0dc4fd3aa128d603a5a12ece33ebaa76622
jer3k <99355997+jer3k@users.noreply.github.com>
2024-04-11 15:27:57 -0700

    flexible dashboard displays

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,149 +1,141 @@
 <template>
   <ReportSelectionManager />
-  <v-container class="d-flex justify-center fill-height" fluid>
-    <v-row class="mt-3">
-      <v-col>
-        <h2 data-testid="legal-name">Welcome, {{ userInfo?.legalName }}.</h2>
-        <v-divider class="mt-2"></v-divider>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <p class="mt-4 mb-4">
-          This tool will help you generate a Pay Transparency report in
-          compliance with the
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/23018"
-            >Pay Transparency Act (gov.bc.ca)</a
-          >
-          and the
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/225_2023"
-            >Pay Transparency Regulation (gov.bc.ca)</a
-          >. The report can be saved for posting on your webpage or in your
-          workplace.
-        </p>
-        <p class="mb-6">
-          Once your CSV file is ready, click on the button below to upload the
-          file and generate a report.
-        </p>
-        <p class="text-caption mb-2">
-          This application does not collect, record or publish personal
-          information.
-        </p>
-        <v-btn class="mb-4" color="primary" to="generate-report-form"
-          >Generate Pay Transparency Report</v-btn
+  <v-row class="mt-3">
+    <v-col>
+      <h2 data-testid="legal-name">Welcome, {{ userInfo?.legalName }}.</h2>
+      <v-divider class="mt-2"></v-divider>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col>
+      <p class="mt-4 mb-4">
+        This tool will help you generate a Pay Transparency report in compliance
+        with the
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/23018"
+          >Pay Transparency Act (gov.bc.ca)</a
         >
-      </v-col>
-      <v-col cols="4">
-        <v-card min-height="70%" class="no-box-shadow">
-          <v-card-text class="generate-report pl-8 pr-8 pt-12 pb-12">
-            <div class="text-left bluebox-width">
-              <strong
-                >For more information on Pay Transparency reporting, please
-                visit
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www2.gov.bc.ca/gov/content/gender-equity/preparing-pay-transparency-reports"
-                  >Guidance for preparing pay transparency reports - Province of
-                  British Columbia (gov.bc.ca)</a
-                >.</strong
+        and the
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/225_2023"
+          >Pay Transparency Regulation (gov.bc.ca)</a
+        >. The report can be saved for posting on your webpage or in your
+        workplace.
+      </p>
+      <p class="mb-6">
+        Once your CSV file is ready, click on the button below to upload the
+        file and generate a report.
+      </p>
+      <p class="text-caption mb-2">
+        This application does not collect, record or publish personal
+        information.
+      </p>
+      <v-btn class="mb-4" color="primary" to="generate-report-form">
+        Generate Pay Transparency Report
+      </v-btn>
+    </v-col>
+    <v-col md="4" cols="12">
+      <v-card class="rounded-lg">
+        <v-toolbar color="primary">
+          <v-toolbar-title>Updates</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text class="mt-4 mb-4">
+          <div class="text-left bluebox-width">
+            <strong>
+              For more information on Pay Transparency reporting, please visit
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www2.gov.bc.ca/gov/content/gender-equity/preparing-pay-transparency-reports"
               >
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row class="mt-8 mb-8">
-      <v-col>
-        <v-card class="rounded-lg" min-height="100%">
-          <v-toolbar color="primary">
-            <v-toolbar-title>Submitted Reports</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text class="mt-4 mb-4">
-            <div v-if="!reports.length">No generated reports yet.</div>
-            <div v-if="reports.length">
-              <v-row>
-                <v-col class="font-weight-bold">Reporting Year</v-col>
-                <v-col class="font-weight-bold">Submission Date</v-col>
-                <v-col class="font-weight-bold" cols="4">Action</v-col>
-              </v-row>
-              <hr class="mt-4 mb-4" />
-              <template v-for="report in reports">
-                <v-row>
-                  <v-col
-                    :data-testid="'reporting_year-' + report.report_id"
-                    >{{ report.reporting_year }}</v-col
-                  >
-                  <v-col
+                Guidance for preparing pay transparency reports - Province of
+                British Columbia (gov.bc.ca)</a
+              >.
+            </strong>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row class="mt-8 mb-8">
+    <v-col>
+      <v-card class="rounded-lg" min-height="100%">
+        <v-toolbar color="primary">
+          <v-toolbar-title>Submitted Reports</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <div v-if="!reports.length">No generated reports yet.</div>
+          <div v-if="reports.length">
+            <v-table>
+              <thead>
+                <tr>
+                  <th class="font-weight-bold">Reporting Year</th>
+                  <th class="font-weight-bold">Submission Date</th>
+                  <th class="font-weight-bold text-right pr-8">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="report in reports" :key="report.report_id">
+                  <td :data-testid="'reporting_year-' + report.report_id">
+                    {{ report.reporting_year }}
+                  </td>
+                  <td
                     :data-testid="'report_published_date-' + report.report_id"
-                    >{{ formatDateTime(report.create_date) }}</v-col
                   >
-                  <v-col cols="4">
-                    <a
+                    {{ formatDateTime(report.create_date) }}
+                  </td>
+                  <td
+                    :data-testid="'report_published_date-' + report.report_id"
+                    class="text-right"
+                  >
+                    <v-btn
                       :data-testid="'view-report-' + report.report_id"
-                      class="pr-5"
-                      href="#"
+                      prepend-icon="mdi-eye-outline"
+                      variant="text"
+                      color="#1976d2"
                       @click="viewReport(report)"
                     >
-                      <v-icon color="#1976d2" icon="mdi-eye-outline"></v-icon>
                       View
-                    </a>
-                    <a
-                      :data-testid="'edit-report-' + report.report_id"
-                      @click="editReport(report)"
+                    </v-btn>
+                    <v-btn
                       v-if="report.is_unlocked"
+                      :data-testid="'edit-report-' + report.report_id"
+                      prepend-icon="mdi-eye-outline"
+                      variant="text"
+                      color="#1976d2"
+                      @click="editReport(report)"
                     >
-                      <v-icon color="#1976d2" icon="mdi-table-edit"></v-icon>
                       Edit
-                    </a>
-                  </v-col>
-                </v-row>
-                <hr class="mt-4 mb-4" />
-              </template>
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="4">
-        <v-row>
-          <v-col>
-            <v-card class="rounded-lg">
-              <v-toolbar color="primary">
-                <v-toolbar-title>Sample CSV</v-toolbar-title>
-              </v-toolbar>
-              <v-card-text class="mt-4 mb-4">
-                <p class="mb-4">
-                  View a sample comma-separated value (CSV) file showing the
-                  required format for pay transparency data uploads.
-                </p>
-                <v-btn color="tertiary" href="SampleCsv.csv" download>
-                  Download sample CSV
-                </v-btn>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <v-card class="rounded-lg">
-              <v-toolbar color="primary">
-                <v-toolbar-title>Updates</v-toolbar-title>
-              </v-toolbar>
-              <v-card-text class="mt-4 mb-4">
-                <p>There are no updates at this time</p>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+                    </v-btn>
+                  </td>
+                </tr>
+              </tbody>
+            </v-table>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col md="4" cols="12">
+      <v-card class="rounded-lg">
+        <v-toolbar color="primary">
+          <v-toolbar-title>Sample CSV</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text class="mt-4 mb-4">
+          <p class="mb-4">
+            View a sample comma-separated value (CSV) file showing the required
+            format for pay transparency data uploads.
+          </p>
+          <v-btn color="tertiary" href="SampleCsv.csv" download>
+            Download sample CSV
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
@@ -172,12 +164,16 @@ export default {
   data: (): DashboardData => ({
     reports: [],
   }),
-  watch: {},
   computed: {
     ...mapState(useReportStepperStore, ['reportId']),
     ...mapState(authStore, ['userInfo']),
     ...mapState(useCodeStore, ['naicsCodes']),
     ...mapState(useConfigStore, ['config']),
+  },
+  async beforeMount() {
+    this.reset();
+    this.loadConfig();
+    this.getReports();
   },
   methods: {
     ...mapActions(useReportStepperStore, ['setReportInfo', 'reset', 'setMode']),
@@ -209,11 +205,6 @@ export default {
       await this.$router.push({ path: 'generate-report-form' });
     },
   },
-  async beforeMount() {
-    this.reset();
-    this.loadConfig();
-    this.getReports();
-  },
 };
 </script>
 
@@ -227,13 +218,5 @@ export default {
 
 .v-card--variant-elevated.no-box-shadow {
   box-shadow: none;
-}
-
-.bluebox-width {
-  max-width: 260px;
-}
-
-.text-left {
-  text-align: left;
 }
 </style>
