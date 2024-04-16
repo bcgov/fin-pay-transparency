bb5394068d3738c600526933a0d9a81efffd8408
Brock Anderson <brock@bandersgeo.ca>
2024-04-15 20:30:11 -0700

    updated alert colours.  changed the class names from 'bootstrap-<alert-type>' to 'alert-<alert-type>'

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -92,28 +92,28 @@ h1 {
   padding-left: 0;
 }
 
-.v-alert.bootstrap-success {
-  color: #2d4821;
-  background-color: #dff0d8 !important;
-  border-color: #d6e9c6 !important;
+.v-alert.alert-success {
+  color: #2d2d2d;
+  background-color: #f6fff8 !important;
+  border-color: #42814a !important;
 }
 
-.v-alert.text-info {
-  color: #313132 !important;
-  background-color: #d9eaf7 !important;
-  border-color: #b8d4ee !important;
+.v-alert.alert-info {
+  color: #2d2d2d !important;
+  background-color: #f7f9fc !important;
+  border-color: #053662 !important;
 }
 
-.v-alert.bootstrap-warning {
-  color: #6c4a00;
-  background-color: #f9f1c6 !important;
-  border-color: #faebcc !important;
+.v-alert.alert-warning {
+  color: #2d2d2d;
+  background-color: #fef1d8 !important;
+  border-color: #f8bb47 !important;
 }
 
-.v-alert.bootstrap-error {
-  color: #a12622;
-  background-color: #f2dede !important;
-  border-color: #ebccd1 !important;
+.v-alert.alert-error {
+  color: #2d2d2d;
+  background-color: #f4e1e2 !important;
+  border-color: #ce3e39 !important;
 }
 
 @media screen and (max-width: 370px) {
