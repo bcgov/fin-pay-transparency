3ce697f8817fe7424bbcd83873e5ae22dc3f6f7e
Sukanya Rath <sukanyarath1987@gmail.com>
2024-04-25 19:16:44 -0700

    chore: move connection pool size to configuration
    Signed-off-by: Sukanya Rath <sukanyarath1987@gmail.com>

--- backend/src/v1/prisma/prisma-client-readonly-replica.ts
+++ backend/src/v1/prisma/prisma-client-readonly-replica.ts
@@ -15,8 +15,9 @@ const DB_PWD = encodeURIComponent(
 const DB_PORT = process.env.POSTGRESQL_PORT || 5432;
 const DB_NAME = process.env.POSTGRESQL_DATABASE || 'postgres';
 const DB_SCHEMA = process.env.DB_SCHEMA || 'pay_transparency';
+const DB_CONNECTION_POOL_SIZE = process.env.DB_CONNECTION_POOL_SIZE || 5;
 const READ_ONLY_REPLICA_HOST = process.env.READ_ONLY_REPLICA_HOST || process.env.POSTGRESQL_HOST || 'localhost';
-const readReplicaUrl = `postgresql://${DB_USER}:${DB_PWD}@${READ_ONLY_REPLICA_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`;
+const readReplicaUrl = `postgresql://${DB_USER}:${DB_PWD}@${READ_ONLY_REPLICA_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=${DB_CONNECTION_POOL_SIZE}`;
 logger.silly(`Connecting to readonly replica at ${readReplicaUrl}`);
 
 const prismaReadOnlyReplica = prisma.$extends(
