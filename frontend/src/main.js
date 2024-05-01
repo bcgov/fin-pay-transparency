f22044716b4889f2c4340f530148aba420abe15c
goemen <gnthomiwa@gmail.com>
2024-05-01 16:28:12 -0700

    add snowplow url at build time

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -18,6 +18,9 @@ import 'viewerjs/dist/viewer.css';
 import component from 'v-viewer';
 import VueDOMPurifyHTML from 'vue-dompurify-html';
 
+import { initializeSnowplow } from './snowplow';
+initializeSnowplow(import.meta.env.VITE_SNOWPLOW_URL);
+
 const myCustomLightTheme = {
   dark: false,
   colors: {
