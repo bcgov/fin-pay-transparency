c001ca121e7f05e64ebdd61367639862b5a313ed
goemen <gnthomiwa@gmail.com>
2024-05-21 10:34:39 -0700

    fix swagger doc

--- backend-external/src/v1/routes/pay-transparency-routes.ts
+++ backend-external/src/v1/routes/pay-transparency-routes.ts
@@ -66,7 +66,7 @@ const router = express.Router();
  *           type: number
  *         employee_count_range:
  *           type: string
- *         data:
+ *         calculated_data:
  *           type: array
  *           items:
  *             $ref: "#/components/schemas/CalculatedData"
