20a59e04fb84cf1e07f6d56fc4069dbaf7b84421
Brock Anderson <brock@bandersgeo.ca>
2024-05-23 15:23:04 -0700

    add support for idir login.  logout partly works, but still needs redirect_uri to be registered with the IDP

---
+++ backend/src/v1/routes/admin-auth-routes.ts
@@ -0,0 +1,207 @@
+import express, { NextFunction, Request, Response } from 'express';
+import passport from 'passport';
+import { v4 as uuidv4 } from 'uuid';
+import { config } from '../../config';
+import { logger as log } from '../../logger';
+import { LogoutReason, adminAuth } from '../services/admin-auth-service';
+import { utils } from '../services/utils-service';
+
+import { body, validationResult } from 'express-validator';
+import {
+  MISSING_TOKENS_ERROR,
+  OIDC_IDIR_CALLBACK_NAME,
+  OIDC_IDIR_STRATEGY_NAME,
+} from '../../constants';
+
+const router = express.Router();
+
+router.get(
+  `/${OIDC_IDIR_CALLBACK_NAME}`,
+  passport.authenticate(OIDC_IDIR_STRATEGY_NAME, {
+    failureMessage: true,
+  }),
+  utils.asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      log.debug(`Login flow callback idir is called.`);
+      const logoutReason = await adminAuth.handleCallBackIdir(req);
+      if (logoutReason == LogoutReason.Login)
+        return res.redirect(config.get('server:adminFrontend'));
+      else return logoutHandler(req, res, next, logoutReason);
+    },
+  ),
+);
+
+//a prettier way to handle errors
+router.get('/error', (req, res) => {
+  log.error(`Login flow Error happened`);
+  res.redirect(config.get('server:adminFrontend') + '/login-error');
+});
+
+function addBaseRouterGet(strategyName, callbackURI) {
+  router.get(
+    callbackURI,
+    passport.authenticate(strategyName, {
+      failureRedirect: 'error',
+    }),
+  );
+}
+
+addBaseRouterGet(OIDC_IDIR_STRATEGY_NAME, '/login_idir');
+
+//removes tokens and destroys session
+async function logoutHandler(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  reason: LogoutReason,
+) {
+  // @ts-expect-error, it is given by passport lib.
+  const idToken: string = req['user']?.idToken;
+  const discovery = await utils.getOidcDiscovery();
+  req.logout(function (err) {
+    console.log('admin logout');
+    if (err) {
+      return next(err);
+    }
+    req.session.destroy((err) => {
+      if (err) {
+        log.error(`Logout failed - ${err.message}`);
+        return next(err);
+      }
+    });
+
+    if (idToken) {
+      let url = '';
+
+      if (reason == 'sessionExpired') url = '/session-expired';
+      else if (reason == 'loginError') url = '/login-error';
+      else if (reason == LogoutReason.LoginIdir)
+        url = '/admin-api/auth/login_idir';
+      else if (reason == 'contactError') url = '/contact-error';
+      else url = '/logout';
+
+      const postLogoutRedirectUri = config.get('server:adminFrontend') + url;
+
+      const retUrl = encodeURIComponent(
+        discovery.end_session_endpoint +
+          '?post_logout_redirect_uri=' +
+          postLogoutRedirectUri +
+          '&id_token_hint=' +
+          idToken,
+      );
+
+      res.redirect(config.get('siteMinder_logout_endpoint') + retUrl);
+    } else {
+      res.redirect(
+        config.get('server:adminFrontend') + '/admin-api/auth/login_idir',
+      );
+    }
+  });
+}
+
+router.get(
+  '/logout',
+  utils.asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      let reason: LogoutReason = LogoutReason.Default;
+      if (req.query?.sessionExpired) reason = LogoutReason.SessionExpired;
+      else if (req.query?.loginError) reason = LogoutReason.LoginError;
+      else if (req.query?.loginIdir) reason = LogoutReason.LoginIdir;
+      else if (req.query?.contactError) reason = LogoutReason.ContactError;
+      await logoutHandler(req, res, next, reason);
+    },
+  ),
+);
+
+const UnauthorizedRsp = {
+  error: 'Unauthorized',
+  error_description: 'Not logged in',
+};
+
+//refreshes jwt on refresh if refreshToken is valid
+router.post(
+  '/refresh',
+  [body('refreshToken').exists()],
+  utils.asyncHandler(async (req: Request, res: Response) => {
+    const user: any = req.user;
+    const session: any = req.session;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      log.error(JSON.stringify(errors));
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    if (!user?.refreshToken || !user?.jwt) {
+      log.error(MISSING_TOKENS_ERROR);
+      res.status(401).json(UnauthorizedRsp);
+    } else if (adminAuth.isTokenExpired(user.jwt)) {
+      if (user?.refreshToken && adminAuth.isRenewable(user.refreshToken)) {
+        return generateTokens(req, res);
+      } else {
+        res.status(401).json(UnauthorizedRsp);
+      }
+    } else {
+      const responseJson = {
+        jwtFrontend: user.jwtFrontend,
+        correlationID: session.correlationID,
+      };
+      return res.status(200).json(responseJson);
+    }
+  }),
+);
+
+//provides a jwt to authenticated users
+router.get(
+  '/token',
+  utils.asyncHandler(adminAuth.refreshJWT),
+  (req: Request, res: Response) => {
+    console.log('/admin-api/auth/token');
+    const user: any = req.user;
+    const session: any = req.session;
+    if (user?.jwtFrontend && user?.refreshToken) {
+      if (session?.passport?.user?._json) {
+        req.session['correlationID'] = uuidv4();
+        log.info(
+          `created correlation id and stored in session for user guid: ${session?.passport?.user?._json?.bceid_user_guid}, user name: ${session?.passport?.user?._json?.bceid_username}, correlation_id:  ${session.correlationID}`,
+        );
+      }
+      const responseJson = {
+        jwtFrontend: user.jwtFrontend,
+        correlationID: session.correlationID,
+      };
+      res.status(200).json(responseJson);
+    } else {
+      log.error(JSON.stringify(UnauthorizedRsp));
+      res.status(401).json(UnauthorizedRsp);
+    }
+  },
+);
+
+async function generateTokens(req: Request, res: Response) {
+  const user: any = req.user;
+  const session: any = req.session;
+  const result = await adminAuth.renew(user.refreshToken);
+  if (result?.jwt && result?.refreshToken) {
+    user.jwt = result.jwt;
+    user.refreshToken = result.refreshToken;
+    user.jwtFrontend = adminAuth.generateUiToken();
+    const responseJson = {
+      jwtFrontend: user.jwtFrontend,
+      correlationID: session.correlationID,
+    };
+    res.status(200).json(responseJson);
+  } else {
+    res.status(401).json(UnauthorizedRsp);
+  }
+}
+
+//redirects to the SSO login screen
+router.get(
+  '/login',
+  passport.authenticate(OIDC_IDIR_STRATEGY_NAME, {
+    failureRedirect: 'error',
+  }),
+);
+export default router;
---
+++ backend/src/v1/services/admin-auth-service.ts
@@ -0,0 +1,245 @@
+import { pay_transparency_company } from '@prisma/client';
+import axios from 'axios';
+import { NextFunction, Request, Response } from 'express';
+import HttpStatus from 'http-status-codes';
+import jsonwebtoken, { JwtPayload, SignOptions } from 'jsonwebtoken';
+import qs from 'querystring';
+import { config } from '../../config';
+import { KEYCLOAK_IDP_HINT_IDIR } from '../../constants';
+import { logger as log } from '../../logger';
+import prisma from '../prisma/prisma-client';
+import { utils } from './utils-service';
+
+enum LogoutReason {
+  Login = 'login', // ie. don't log out
+  Default = 'default',
+  SessionExpired = 'sessionExpired',
+  LoginError = 'loginError',
+  LoginIdir = 'loginIdir',
+  ContactError = 'contactError',
+}
+
+let kcPublicKey: string;
+const adminAuth = {
+  // Check if JWT Access Token has expired
+  isTokenExpired(token: string) {
+    const now = Date.now().valueOf() / 1000;
+    const payload = jsonwebtoken.decode(token);
+
+    return !!payload['exp'] && payload['exp'] < now + 30; // Add 30 seconds to make sure , edge case is avoided and token is refreshed.
+  },
+
+  // Check if JWT Refresh Token has expired
+  isRenewable(token: string) {
+    const now = Date.now().valueOf() / 1000;
+    const payload: JwtPayload = jsonwebtoken.decode(token) as JwtPayload;
+
+    // Check if expiration exists, or lacks expiration
+    return (
+      (typeof payload.exp !== 'undefined' &&
+        payload.exp !== null &&
+        payload.exp === 0) ||
+      payload.exp > now
+    );
+  },
+
+  // Get new JWT and Refresh tokens
+  async renew(refreshToken: string) {
+    let result: any = {};
+
+    try {
+      const discovery = await utils.getOidcDiscovery();
+      const response = await axios.post(
+        discovery.token_endpoint,
+        qs.stringify({
+          client_id: config.get('oidc:clientId'),
+          client_secret: config.get('oidc:clientSecret'),
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken,
+          scope: 'bceidbusiness',
+        }),
+        {
+          headers: {
+            Accept: 'application/json',
+            'Cache-Control': 'no-cache',
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        },
+      );
+
+      log.verbose('renew', utils.prettyStringify(response.data));
+      if (response.data?.access_token && response.data?.refresh_token) {
+        result.jwt = response.data.access_token;
+        result.refreshToken = response.data.refresh_token;
+        result.idToken = response.data.id_token;
+      } else {
+        log.error('Access token or refresh token not retrieved properly');
+      }
+    } catch (error) {
+      log.error('renew', error.message);
+      result = error.response?.data;
+    }
+
+    return result;
+  },
+
+  // Update or remove token based on JWT and user state
+  async refreshJWT(req: Request, _res: Response, next: NextFunction) {
+    const user: any = req.user;
+    try {
+      if (user?.jwt) {
+        log.verbose('refreshJWT', 'User & JWT exists');
+
+        if (adminAuth.isTokenExpired(user.jwt)) {
+          log.verbose('refreshJWT', 'JWT has expired');
+
+          if (!!user.refreshToken && adminAuth.isRenewable(user.refreshToken)) {
+            log.verbose('refreshJWT', 'Can refresh JWT token');
+
+            // Get new JWT and Refresh Tokens and update the request
+            const result: any = await adminAuth.renew(user.refreshToken);
+            user.jwt = result.jwt; // eslint-disable-line require-atomic-updates
+            user.refreshToken = result.refreshToken; // eslint-disable-line require-atomic-updates
+          } else {
+            log.verbose('refreshJWT', 'Cannot refresh JWT token');
+            delete req.user;
+          }
+        }
+      } else {
+        log.verbose('refreshJWT', 'No existing User or JWT');
+        delete req.user;
+      }
+    } catch (error) {
+      log.error('refreshJWT', error.message);
+    }
+    next();
+  },
+
+  generateUiToken() {
+    const i = config.get('tokenGenerate:issuer');
+    const s = 'user@finpaytransparency.ca';
+    const a = config.get('server:frontend');
+    const signOptions: SignOptions = {
+      issuer: i,
+      subject: s,
+      audience: a,
+      expiresIn: '30m',
+      algorithm: 'RS256',
+    };
+
+    const privateKey = config.get('tokenGenerate:privateKey');
+    const uiToken = jsonwebtoken.sign({}, privateKey, signOptions);
+    log.verbose('Generated JWT', uiToken);
+    return uiToken;
+  },
+
+  /**
+   * check if the jwt contains valid claims.
+   * The token should be issued by azureidir and the audience should be the configured client id
+   * Since the app is using the standard realm of Keycloak, this extra check is required to make sure other users are not able to get access.
+   * https://github.com/bcgov/sso-keycloak/wiki/Using-Your-SSO-Client#do-validate-the-idp-in-the-jwt
+   * @param jwt the token
+   */
+  validateClaims: function (jwt: any) {
+    const payload: JwtPayload = jsonwebtoken.decode(jwt) as JwtPayload;
+    if (payload?.identity_provider !== KEYCLOAK_IDP_HINT_IDIR) {
+      throw new Error(
+        `backend token invalid, identity_provider is not ${KEYCLOAK_IDP_HINT_IDIR}`,
+        jwt,
+      );
+    }
+    console.log(`oicd:adminClientId:` + config.get('oidc:adminClientId'));
+    console.log(payload?.aud);
+    if (payload?.aud !== config.get('oidc:adminClientId')) {
+      throw new Error(
+        'backend token invalid, aud claim validation failed',
+        jwt,
+      );
+    }
+    return true;
+  },
+
+  isValidBackendToken() {
+    return async function (req: Request, res: Response, next: NextFunction) {
+      const session: any = req.session;
+      if (!kcPublicKey) {
+        kcPublicKey = await utils.getKeycloakPublicKey();
+        if (!kcPublicKey) {
+          log.error('error is from getKeycloakPublicKey');
+          return res.status(HttpStatus.UNAUTHORIZED).json();
+        }
+      }
+      if (session?.passport?.user?.jwt) {
+        const jwt = session.passport.user.jwt;
+        try {
+          jsonwebtoken.verify(jwt, kcPublicKey);
+          adminAuth.validateClaims(jwt);
+          return next();
+        } catch (e) {
+          log.error('error is from verify', e);
+          return res.status(HttpStatus.UNAUTHORIZED).json();
+        }
+      } else {
+        log.silly(req.session);
+        log.silly('no jwt responding back 401');
+        return res.status(HttpStatus.UNAUTHORIZED).json();
+      }
+    };
+  },
+  async getUserInfo(req: Request, res: Response) {
+    const session: any = req.session;
+    const userInfo = session?.passport?.user;
+    if (!userInfo?.jwt || !userInfo?._json) {
+      return res.status(HttpStatus.UNAUTHORIZED).json({
+        message: 'No session data',
+      });
+    }
+    const userInfoFrontend = {
+      displayName: userInfo._json.display_name,
+      ...session.companyDetails,
+    };
+    return res.status(HttpStatus.OK).json(userInfoFrontend);
+  },
+
+  async storeUserInfo(companyDetails, userInfo) {
+    if (!userInfo?.jwt || !userInfo?._json) {
+      throw new Error('No session data');
+    }
+    try {
+      await prisma.$transaction(async (tx) => {
+        const companyRecord: pay_transparency_company =
+          this.companyDetailsToRecord(
+            companyDetails,
+            userInfo._json.bceid_business_guid,
+          );
+        await this.createOrUpdatePayTransparencyCompany(companyRecord, tx);
+        await this.createOrUpdatePayTransparencyUser(userInfo, tx);
+      });
+    } catch (e) {
+      log.error(e);
+      throw new Error('Error while storing user info');
+    }
+  },
+
+  async handleCallBackIdir(req: Request): Promise<LogoutReason> {
+    const session: any = req.session;
+    const userInfo = utils.getSessionUser(req);
+    const jwtPayload = jsonwebtoken.decode(userInfo.jwt) as JwtPayload;
+    const idirUserGuid = jwtPayload?.idir_user_guid;
+    if (!idirUserGuid) {
+      log.error(`no idir_user_guid found in the jwt token`, userInfo.jwt);
+      return LogoutReason.LoginError;
+    }
+
+    try {
+      adminAuth.validateClaims(userInfo.jwt);
+    } catch (e) {
+      log.error('invalid claims in token', e);
+      return LogoutReason.LoginError;
+    }
+
+    return LogoutReason.Login;
+  },
+};
+
+export { LogoutReason, adminAuth };
