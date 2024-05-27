b322ecf299e2763b79435870e8761b7f06d7cbb2
Brock Anderson <brock@bandersgeo.ca>
2024-05-27 09:08:51 -0700

    made the (preliminary) login page slightly better.  It will still need to be redesigned eventually.

--- admin-frontend/src/components/Login.vue
+++ admin-frontend/src/components/Login.vue
@@ -1,14 +1,19 @@
 <template>
-  Login page placeholder
-  <v-btn
-    id="login-button"
-    class="btn-primary"
-    data-testid="login-button"
-    @click="clearStorageAndRedirectToLogin"
-    title="Login"
-  >
-    Log in with IDIR
-  </v-btn>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col xs="4" sm="4" md="4" lg="4" xl="4">
+        <v-btn
+          id="login-button"
+          class="btn-primary"
+          data-testid="login-button"
+          @click="clearStorageAndRedirectToLogin"
+          title="Login"
+        >
+          Log in with IDIR
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
