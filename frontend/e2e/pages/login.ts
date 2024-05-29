219ef9544d4138ea98a015765e0e0dd23606e039
Goeme Nthomiwa <gnthomiwa@gmail.com>
2024-05-29 11:05:03 -0700

    feat: geo 567 integrate delete reports teardown in the playwright tests (#507)
    Co-authored-by: Sukanya Rath <98050194+sukanya-rath@users.noreply.github.com>

--- frontend/e2e/pages/login.ts
+++ frontend/e2e/pages/login.ts
@@ -19,6 +19,9 @@ export class LoginPage extends PTPage {
     const html = await this.instance.locator('body').innerHTML();
     if (html.includes('Use a Business BCeID')) {
       expect(this.instance.getByText('Use a Business BCeID'));
+      /**
+       * Make sure to use test account to prevent the teardown from all deleting your local development reports 
+       */
       await this.instance.fill('#user', process.env.E2E_USERNAME!);
       await this.instance.click('#password');
       await this.instance.fill('#password', process.env.E2E_PASSWORD!);
