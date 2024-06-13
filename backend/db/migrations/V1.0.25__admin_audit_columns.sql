700a102f034f35d0441f84db18d0e2e2c48588c1
goemen <gnthomiwa@gmail.com>
2024-06-13 13:39:27 -0700

    add report history audit columns

--- backend/db/migrations/V1.0.25__admin_audit_columns.sql
+++
@@ -1,4 +0,0 @@
-SET search_path TO pay_transparency;
-
-alter table pay_transparency_report add column idir_modified_username varchar(255) null;
-alter table pay_transparency_report add column idir_modified_date timestamp null;
