819a6d6850c1d66001893e4a8f439b72c1306468
jer3k <99355997+jer3k@users.noreply.github.com>
2024-06-20 16:17:16 -0700

    Store error records

--- backend/src/v1/routes/file-upload-routes.spec.ts
+++ backend/src/v1/routes/file-upload-routes.spec.ts
@@ -1,16 +1,32 @@
 import express, { Application } from 'express';
 import request from 'supertest';
 import { fileUploadRouter } from './file-upload-routes';
+import { SubmissionError } from '../services/file-upload-service';
 let app: Application;
 
+// Mock the module, but only override handleSubmission
 const mockHandleSubmission = jest.fn().mockResolvedValue({});
-const mockGetCompanies = jest.fn();
-jest.mock('../services/file-upload-service', () => ({
-  fileUploadService: {
-    handleSubmission: jest.fn((...args) => mockHandleSubmission(...args)),
-    getCompanies: () => mockGetCompanies(),
+jest.mock('../services/file-upload-service', () => {
+  const actualFileUploadService = jest.requireActual(
+    '../services/file-upload-service',
+  );
+  return {
+    ...actualFileUploadService,
+    fileUploadService: {
+      ...actualFileUploadService.fileUploadService,
+      handleSubmission: jest.fn((...args) => mockHandleSubmission(...args)),
+    },
+  };
+});
+
+const mockStoreError = jest.fn();
+jest.mock('../services/error-service', () => ({
+  errorService: {
+    storeError: () => mockStoreError(),
+    retrieveErrors: jest.fn(),
   },
 }));
+
 describe('file-upload', () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -19,8 +35,21 @@ describe('file-upload', () => {
   });
 
   describe('/', () => {
-    it('/ [POST] - should return 200', () => {
-      return request(app).post('/');
+    it('/ [POST] - should return 200', async () => {
+      await request(app).post('/').expect(200);
+      expect(mockStoreError).not.toHaveBeenCalled();
+    });
+    it('/ [POST] - should return 400', async () => {
+      mockHandleSubmission.mockResolvedValue(new SubmissionError('test'));
+      await request(app).post('/').expect(400);
+      expect(mockStoreError).toHaveBeenCalled();
+    });
+    it('/ [POST] - should return 500', async () => {
+      mockHandleSubmission.mockImplementation(() => {
+        throw Error('test');
+      });
+      await request(app).post('/').expect(500);
+      expect(mockStoreError).toHaveBeenCalled();
     });
   });
 });
