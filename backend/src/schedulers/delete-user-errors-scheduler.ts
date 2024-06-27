279ceae9e0a9b07e6d3d057f3fa40f53ba092d07
Brock Anderson <brock@bandersgeo.ca>
2024-06-26 18:01:43 -0700

    feat: env vars to configure user error logging (#557)

---
+++ backend/src/schedulers/delete-user-errors-scheduler.ts
@@ -0,0 +1,33 @@
+import { DateTimeFormatter, LocalDate, ZoneId } from '@js-joda/core';
+import advisoryLock from 'advisory-lock';
+import { config } from '../config';
+import { logger as log } from '../logger';
+import { errorService } from '../v1/services/error-service';
+import { createJob } from './create-job';
+
+const SCHEDULER_NAME = 'DeleteUserErrors';
+const mutex = advisoryLock(config.get('server:databaseUrl'))(
+  `${SCHEDULER_NAME}-lock`,
+);
+const crontime = config.get('server:userErrorLogging:deleteScheduleCronTime');
+const numMonthsOfErrorsToKeep = config.get(
+  'server:userErrorLogging:numMonthsOfUserErrorsToKeep',
+);
+
+export default createJob(
+  crontime,
+  async () => {
+    log.info(`Starting scheduled job '${SCHEDULER_NAME}'.`);
+    const thresholdDate = LocalDate.now(ZoneId.UTC)
+      .atStartOfDay(ZoneId.UTC)
+      .minusMonths(numMonthsOfErrorsToKeep)
+      .format(DateTimeFormatter.ISO_DATE_TIME);
+    await errorService.deleteErrorsOlderThan(thresholdDate);
+    log.info(`Completed scheduled job '${SCHEDULER_NAME}'.`);
+  },
+  mutex,
+  {
+    title: `Error in ${SCHEDULER_NAME}`,
+    message: `Error in scheduled job: ${SCHEDULER_NAME}`,
+  },
+);
