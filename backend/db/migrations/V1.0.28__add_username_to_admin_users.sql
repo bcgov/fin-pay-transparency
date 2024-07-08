8403f274f711225f60c9cf64d98934d3ea0fcded
goemen <gnthomiwa@gmail.com>
2024-07-08 11:07:40 -0700

    rename migration file

--- backend/db/migrations/V1.0.28__add_username_to_admin_users.sql
+++
@@ -1,4 +0,0 @@
-SET search_path TO pay_transparency;
-
-ALTER TABLE admin_user ADD COLUMN IF NOT EXISTS username VARCHAR(255) NOT NULL DEFAULT '';
-ALTER TABLE admin_user_history ADD COLUMN IF NOT EXISTS username VARCHAR(255) NOT NULL DEFAULT '';
