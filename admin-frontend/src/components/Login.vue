9cf250a79ce03fdd1b1430aaf82787c3b5ed39ad
goemen <gnthomiwa@gmail.com>
2024-07-09 13:18:03 -0700

    using auth page layout

--- admin-frontend/src/components/Login.vue
+++ admin-frontend/src/components/Login.vue
@@ -1,51 +1,34 @@
 <template>
-  <v-app-bar absolute style="z-index: 1002" class="d-flex justify-center">
-    <v-container>
-      <span class="text-primary page-title">Gender Equity Office</span>
-    </v-container>
-  </v-app-bar>
-  <v-row class="fill-height container">
-    <v-col xs="12" class="d-flex flex-column justify-center align-center action">
-      <div class="d-flex flex-column">
-        <p class="title text-h4">Log in</p>
-        <v-btn
-          id="login-button"
-          class="btn-primary login-button"
-          data-testid="login-button"
-          @click="clearStorageAndRedirectToLogin"
-          title="Login"
-        >
-          Log in with IDIR
-        </v-btn>
-      </div>
-    </v-col>
-    <v-col xs="12" class="justify-center cover d-none d-xs-none d-sm-none d-md-flex"> </v-col>
-  </v-row>
+  <AuthLayout>
+    <template #content>
+      <p class="title text-h4">Log in</p>
+      <v-btn
+        id="login-button"
+        class="btn-primary login-button"
+        data-testid="login-button"
+        @click="clearStorageAndRedirectToLogin"
+        title="Login"
+      >
+        Log in with IDIR
+      </v-btn>
+    </template>
+  </AuthLayout>
+  
 </template>
 
-<script>
+<script setup lang="ts">
+import AuthLayout from './util/AuthLayout.vue';
 import { authStore } from '../store/modules/auth';
 import { AuthRoutes } from '../utils/constant';
 import { sanitizeUrl } from '@braintree/sanitize-url';
 
-export default {
-  name: 'Login',
-  components: {},
-  data() {
-    return {
-      appTitle: 'Pay Transparency Admin Dashboard',
-      authRoutesLogin: sanitizeUrl(AuthRoutes.LOGIN_AZUREIDIR),
-    };
-  },
-  computed: {},
-  methods: {
-    sanitizeUrl: sanitizeUrl,
-    clearStorageAndRedirectToLogin() {
-      authStore().setJwtToken();
-      window.location.href = this.authRoutesLogin;
-    },
-  },
+const appTitle = 'Pay Transparency Admin Dashboard';
+const authRoutesLogin = sanitizeUrl(AuthRoutes.LOGIN_AZUREIDIR);
+const clearStorageAndRedirectToLogin = () => {
+  authStore().setJwtToken();
+  window.location.href = authRoutesLogin;
 };
+
 </script>
 
 <style scoped lang="scss">
--- admin-frontend/src/components/TokenExpired.vue
+++ admin-frontend/src/components/TokenExpired.vue
@@ -1,42 +1,29 @@
 <template>
-  <v-container>
-    <v-row align="center" justify="center">
-      <v-col xs="4" sm="4" md="4" lg="4" xl="4">
+  <AuthLayout>
+    <template #content>
+      <v-container fluid class="root">
+        <h1 class="title mb-2">Session Expired</h1>
         <v-alert dense variant="outlined" class="alert-error mb-3">
           Your session has expired.
         </v-alert>
-        <v-btn class="btn-primary" @click="redirectToLogin">Login Again</v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
+        <v-btn class="btn-primary" @click="clearStorageAndRedirectToLogin">Login Again</v-btn>
+      </v-container>
+    </template>
+  </AuthLayout>
 </template>
 
-<script>
+<script setup lang='ts'>
+import { storeToRefs } from 'pinia';
 import { authStore } from '../store/modules/auth';
 import { mapState } from 'pinia';
 import { AuthRoutes } from '../utils/constant';
 import { sanitizeUrl } from '@braintree/sanitize-url';
+import AuthLayout from './util/AuthLayout.vue';
 
-export default {
-  name: 'TokenExpired',
-  components: {},
-  data() {
-    return {
-      appTitle: 'Pay Transparency Reporting',
-      authRoutesLogin: sanitizeUrl(AuthRoutes.LOGIN_AZUREIDIR),
-    };
-  },
-  computed: {
-    ...mapState(authStore, ['isAuthenticated']),
-  },
-  methods: {
-    redirectToLogin() {
-      authStore().setJwtToken();
-      window.location.href = this.authRoutesLogin;
-    },
-  },
+const authRoutesLogin = sanitizeUrl(AuthRoutes.LOGIN_AZUREIDIR);
+const { isAuthenticated } = storeToRefs(authStore);
+const clearStorageAndRedirectToLogin = () => {
+  authStore().setJwtToken();
+  window.location.href = authRoutesLogin;
 };
 </script>
-
-<styles>
-</styles>
