bfd5e6134f41df75d4f6f765eb51b6566ee3e089
goemen <gnthomiwa@gmail.com>
2024-07-11 07:19:20 -0700

    adding email template

---
+++ backend/src/v1/email-templates/index.ts
@@ -0,0 +1,23 @@
+import { config } from '../../config';
+
+export const EMAIL_TEMPLATES = {
+  USER_INVITE: {
+    subject: 'Pay Transparency Admin Portal Onboarding',
+    title: 'Pay Transparency Admin Portal Onboarding',
+    body: (firstname: string) => `
+        <p>Hello ${firstname},</p>
+        <p>
+        You are invited to join the Pay Transparency Admin Portal as an Admin User.
+        Please click the link
+        <a href="${config.get('server:adminFrontend')}">here</a>
+        to complete your registration and access the application. This invitation
+        expires in
+        ${config.get('server:adminInvitationDurationInHours')}
+        hours after which the Admin Portal Manager will need to add you again.
+        </p>
+        <p>
+        Upon first login, you will be logged out to allow the system to confirm your
+        access. You will then need to log in again.
+        </p>`,
+  },
+};
--- backend/src/v1/services/admin-users-service.spec.ts
+++ backend/src/v1/services/admin-users-service.spec.ts
@@ -1,8 +1,12 @@
 import { faker } from '@faker-js/faker';
 import { AdminUserService } from './admin-users-services';
-import { PTRT_ADMIN_ROLE_NAME, PTRT_USER_ROLE_NAME } from '../../constants/admin';
+import {
+  PTRT_ADMIN_ROLE_NAME,
+  PTRT_USER_ROLE_NAME,
+} from '../../constants/admin';
 
 const mockFindFirst = jest.fn();
+const mockFindFirstOrThrow = jest.fn();
 const mockCreate = jest.fn();
 const mockUpdate = jest.fn();
 jest.mock('../prisma/prisma-client', () => ({
@@ -11,6 +15,7 @@ jest.mock('../prisma/prisma-client', () => ({
   default: {
     admin_user_onboarding: {
       findFirst: (args) => mockFindFirst(args),
+      findFirstOrThrow: (args) => mockFindFirstOrThrow(args),
       create: (args) => mockCreate(args),
       update: (args) => mockUpdate(args),
     },
@@ -49,7 +54,7 @@ describe('admin-users-service', () => {
     });
     describe('when invitation exists', () => {
       it('should send a update invitation and send email', async () => {
-        mockFindFirst.mockResolvedValue({})
+        mockFindFirst.mockResolvedValue({});
         await service.addNewUser(
           faker.internet.email(),
           PTRT_ADMIN_ROLE_NAME,
@@ -59,6 +64,14 @@ describe('admin-users-service', () => {
         expect(mockUpdate).toHaveBeenCalledTimes(1);
         expect(mockSendEmailWithRetry).toHaveBeenCalledTimes(1);
       });
+    });
+  });
+
+  describe('resendInvite', () => {
+    it('should send a new invitation', async () => {
+      mockFindFirstOrThrow.mockResolvedValue({});
+      await service.resendInvite(faker.string.uuid());
+      expect(mockSendEmailWithRetry).toHaveBeenCalledTimes(1);
     });
   });
 });
