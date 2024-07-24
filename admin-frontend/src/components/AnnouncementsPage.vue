11fdddafaf12e275d02b78f48a0d0432c4864177
Brock Anderson <brock@bandersgeo.ca>
2024-07-24 10:26:17 -0700

    feat: Admin - added an 'Add announcement' button (#618)
    Co-authored-by: Sukanya Rath <98050194+sukanya-rath@users.noreply.github.com>

--- admin-frontend/src/components/AnnouncementsPage.vue
+++ admin-frontend/src/components/AnnouncementsPage.vue
@@ -1,5 +1,32 @@
-<template>Announcements placeholder</template>
+<template>
+  <div class="search-results w-100">
+    <v-row class="mt-0 w-100" no-gutters>
+      <v-col sm="8" md="8" lg="6" xl="4" class="d-flex align-center"> </v-col>
+      <v-col
+        sm="4"
+        md="4"
+        lg="6"
+        xl="8"
+        class="d-flex justify-end align-center"
+      >
+        <v-btn class="btn-primary" @click="addAnnouncement()">
+          Add announcement
+        </v-btn>
+      </v-col>
+    </v-row>
+  </div>
+</template>
 
-<script lang="ts"></script>
+<script lang="ts">
+export default {
+  name: 'Announcements',
+};
+</script>
+
+<script setup lang="ts">
+async function addAnnouncement() {
+  console.log('TODO: add announcement');
+}
+</script>
 
 <style></style>
