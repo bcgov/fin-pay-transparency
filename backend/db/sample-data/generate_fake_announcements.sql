b07d25fae4d592e27ba232a9bfe49c0fbd95600c
Brock Anderson <brock@bandersgeo.ca>
2024-07-30 11:49:14 -0700

    fixed the admin_user_id

--- backend/db/sample-data/generate_fake_announcements.sql
+++ backend/db/sample-data/generate_fake_announcements.sql
@@ -13,17 +13,17 @@ begin
 
   insert into pay_transparency.announcement (title, description, published_on, expires_on, status, created_by, updated_by)
 	values 
-		('announcement 1', '', now(), now() + interval '12 week', 'PUBLISHED', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d'),
-	('announcement 2', '', now() - interval '1 week', now() + interval '12 week', 'DRAFT', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d'),
-    ('announcement 3', '', now() - interval '2 week', now() + interval '10 week', 'PUBLISHED', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d'),
-    ('announcement 4', '', now() - interval '6 week', now() + interval '11 week', 'PUBLISHED', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d'),
-    ('announcement 5', '', now() - interval '7 week', now() + interval '14 week', 'DRAFT', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d'),
-	('announcement 6', '', now(), now() + interval '12 week', 'PUBLISHED', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d'),
-	('announcement 7', '', now() - interval '1 week', now() + interval '12 week', 'DRAFT', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d'),
-    ('announcement 8', '', now() - interval '2 week', now() + interval '11 week', 'PUBLISHED', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d'),
-    ('announcement 9', '', now() - interval '3 week', now() + interval '5 week', 'DRAFT', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d'),
-	('announcement 10', '', now() - interval '2 week', now() + interval '8 week', 'PUBLISHED', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d'),
-    ('announcement 11', '', now() - interval '7 week', now() + interval '13 week', 'DRAFT', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d', '9a5b6928-f8b2-4fba-b520-bfe76b56d28d');
+		('announcement 1', '', now(), now() + interval '12 week', 'PUBLISHED', adminUserId, adminUserId),
+	('announcement 2', '', now() - interval '1 week', now() + interval '12 week', 'DRAFT', adminUserId, adminUserId),
+    ('announcement 3', '', now() - interval '2 week', now() + interval '10 week', 'PUBLISHED', adminUserId, adminUserId),
+    ('announcement 4', '', now() - interval '6 week', now() + interval '11 week', 'PUBLISHED', adminUserId, adminUserId),
+    ('announcement 5', '', now() - interval '7 week', now() + interval '14 week', 'DRAFT', adminUserId, adminUserId),
+	('announcement 6', '', now(), now() + interval '12 week', 'PUBLISHED', adminUserId, adminUserId),
+	('announcement 7', '', now() - interval '1 week', now() + interval '12 week', 'DRAFT', adminUserId, adminUserId),
+    ('announcement 8', '', now() - interval '2 week', now() + interval '11 week', 'PUBLISHED', adminUserId, adminUserId),
+    ('announcement 9', '', now() - interval '3 week', now() + interval '5 week', 'DRAFT', adminUserId, adminUserId),
+	('announcement 10', '', now() - interval '2 week', now() + interval '8 week', 'PUBLISHED', adminUserId, adminUserId),
+    ('announcement 11', '', now() - interval '7 week', now() + interval '13 week', 'DRAFT', adminUserId, adminUserId);
 
 end
 
