4f4c9c9bb99a82c23c904da93ce4601ecae11046
goemen <gnthomiwa@gmail.com>
2024-08-21 10:53:06 -0700

    fix s3 endpoint

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -137,8 +137,8 @@ config.defaults({
   s3: {
     accessKeyId: process.env.S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    region: process.env.S3_REGION || 'ca-central',
-    endpoint: process.env.S3_ENDPOINT,
+    region: process.env.S3_REGION || 'ca-central-1',
+    endpoint: `https://${process.env.S3_ENDPOINT}`,
     bucket: process.env.S3_BUCKET_NAME,
   }
 });
