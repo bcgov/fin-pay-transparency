126782e70a13d8ee97d98f2371a07ed1bedee89f
goemen <gnthomiwa@gmail.com>
2024-08-27 13:38:01 -0700

    ensure announcement is selectable after add

--- admin-frontend/src/components/AddAnnouncementPage.vue
+++ admin-frontend/src/components/AddAnnouncementPage.vue
@@ -14,10 +14,14 @@ import {
 } from '../types/announcements';
 import { useRouter } from 'vue-router';
 import AnnouncementForm from './announcements/AnnouncementForm.vue';
+import { useAnnouncementSelectionStore } from '../store/modules/announcementSelectionStore';
+import { useAnnouncementSearchStore } from '../store/modules/announcementSearchStore';
 import { NotificationService } from '../services/notificationService';
 import ApiService from '../services/apiService';
 
 const router = useRouter();
+const selectionStore = useAnnouncementSelectionStore();
+const announcementSearch = useAnnouncementSearchStore();
 
 const submit = async (data: AnnouncementFormValue) => {
   try {
@@ -25,6 +29,8 @@ const submit = async (data: AnnouncementFormValue) => {
     NotificationService.pushNotificationSuccess(
       'Announcement saved successfully',
     );
+    selectionStore.reset();
+    await announcementSearch.repeatSearch();
     router.push('/announcements');
   } catch (error) {
     console.error('Failed to save announcement', error);
