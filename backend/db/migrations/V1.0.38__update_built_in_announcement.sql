e7e0a961ffcb3c31fb89971c64f4fec97158aa8e
Brock Anderson <brock@bandersgeo.ca>
2024-09-03 13:15:44 -0700

    changed tz of date for default announcement

--- backend/db/migrations/V1.0.38__update_built_in_announcement.sql
+++ backend/db/migrations/V1.0.38__update_built_in_announcement.sql
@@ -1,5 +1,5 @@
 SET search_path TO pay_transparency;
 
 -- add a 'published_on' date to the built-in announcement
-update announcement set published_on = LOCALTIMESTAMP AT TIME ZONE 'UTC-7' 
+update announcement set published_on = LOCALTIMESTAMP AT TIME ZONE 'UTC' 
 where published_on is null and title = 'Guidance for preparing reports';
