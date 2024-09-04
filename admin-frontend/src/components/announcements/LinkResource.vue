113f6070cdcdd748f83ff2f1d9f89e02247af10d
goemen <gnthomiwa@gmail.com>
2024-09-04 10:50:03 -0700

    create link display in announcement edit form

---
+++ admin-frontend/src/components/announcements/LinkResource.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="root">
+    <v-btn
+      variant="text"
+      color="primary"
+      append-icon="mdi-open-in-new"
+      :href="props.url"
+      target="_blank"
+      >{{ props.text }}</v-btn
+    >
+    <v-btn
+      density="compact"
+      icon="mdi-pencil"
+      class="mr-2"
+      aria-label="Edit link"
+      @click="emits('onEdit')"
+    ></v-btn>
+    <v-btn
+      density="compact"
+      icon="mdi-delete-outline"
+      aria-label="Delete link"
+      @click="emits('onDelete')"
+    ></v-btn>
+  </div>
+</template>
+<script setup lang="ts">
+import { defineProps, watch } from 'vue';
+
+type LinkResourceProps = {
+  text: string;
+  url: string;
+};
+
+const props = defineProps<LinkResourceProps>();
+watch(props, () => {
+  console.log(props);
+});
+const emits = defineEmits(['onEdit', 'onDelete']);
+</script>
+<style scoped></style>
