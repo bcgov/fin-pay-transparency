06b1adea81e553c4d7b0371dd8737ed2f18cefd2
Brock Anderson <brock@bandersgeo.ca>
2024-09-04 10:01:06 -0700

    fixed timezone of built-in announcement's published_on date

--- backend/db/migrations/V1.0.38__update_built_in_announcement.sql
+++ backend/db/migrations/V1.0.38__update_built_in_announcement.sql
@@ -1,5 +1,5 @@
 SET search_path TO pay_transparency;
 
 -- add a 'published_on' date to the built-in announcement
-update announcement set published_on = LOCALTIMESTAMP AT TIME ZONE 'UTC' 
+update announcement set published_on = LOCALTIMESTAMP AT TIME ZONE 'UTC-7' 
 where published_on is null and title = 'Guidance for preparing reports';
