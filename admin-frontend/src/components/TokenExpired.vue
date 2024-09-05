e9ccf136578b5b7c42571df822a2550e63e0496a
Brock Anderson <brock@bandersgeo.ca>
2024-09-05 09:03:23 -0700

    fix code smells

--- admin-frontend/src/components/TokenExpired.vue
+++ admin-frontend/src/components/TokenExpired.vue
@@ -2,26 +2,25 @@
   <AuthLayout>
     <template #content>
       <v-container fluid class="root">
-        <h1 class="title mb-2">Session Expired</h1>
-        <v-alert dense variant="outlined" class="alert-error mb-3">
+        <h1 class="text-h6 mb-2">Session Expired</h1>
+        <v-alert density="compact" variant="outlined" class="alert-error mb-3">
           Your session has expired.
         </v-alert>
-        <v-btn class="btn-primary" @click="clearStorageAndRedirectToLogin">Login Again</v-btn>
+        <v-btn class="btn-primary" @click="clearStorageAndRedirectToLogin"
+          >Login Again</v-btn
+        >
       </v-container>
     </template>
   </AuthLayout>
 </template>
 
-<script setup lang='ts'>
-import { storeToRefs } from 'pinia';
+<script setup lang="ts">
 import { authStore } from '../store/modules/auth';
-import { mapState } from 'pinia';
 import { AuthRoutes } from '../utils/constant';
 import { sanitizeUrl } from '@braintree/sanitize-url';
-import AuthLayout from './util/AuthLayout.vue';
 
 const authRoutesLogin = sanitizeUrl(AuthRoutes.LOGIN_AZUREIDIR);
-const { isAuthenticated } = storeToRefs(authStore);
+
 const clearStorageAndRedirectToLogin = () => {
   authStore().setJwtToken();
   window.location.href = authRoutesLogin;
