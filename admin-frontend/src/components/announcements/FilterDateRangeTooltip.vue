040e43bb7d974bc3f6dbe6fa947e32b83db7d014
Goeme Nthomiwa <gnthomiwa@gmail.com>
2024-09-17 13:41:53 -0700

    fix: geo 1142 accessibility announcements filter date range (#757)

---
+++ admin-frontend/src/components/announcements/FilterDateRangeTooltip.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-tooltip
+    :id="id"
+    text="This is a date range selection. Please select the start and end date of the range. For 1 day please click the same date twice"
+    width="300px"
+  >
+    <template #activator="{ props }">
+      <v-icon
+        v-bind="props"
+        icon="fa:fas fa-circle-info"
+        size="x-small"
+        color="primary"
+        class="ml-1"
+        tabindex="0"
+        role="tooltip"
+        :aria-labeledby="id"
+      />
+    </template>
+  </v-tooltip>
+</template>
+<script setup lang="ts">
+import { defineProps } from 'vue';
+defineProps<{ id: string }>();
+</script>
