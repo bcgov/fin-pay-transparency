151f6d81ea79ed96963f63ee14d6a0f83d107eb4
goemen <gnthomiwa@gmail.com>
2024-09-23 09:58:59 -0700

    fix: display error snackbar when download file fails

--- admin-frontend/src/components/announcements/__tests__/AnnouncementItem.spec.ts
+++ admin-frontend/src/components/announcements/__tests__/AnnouncementItem.spec.ts
@@ -36,6 +36,14 @@ vi.mock('../../../services/apiService', () => ({
     downloadFile: (...args) => mockDownloadFile(...args),
   },
 }));
+
+const pushNotificationErrorMock = vi.fn();
+vi.mock('../../../services/notificationService', () => ({
+  NotificationService: {
+    pushNotificationError: (...args) => pushNotificationErrorMock(...args),
+  },
+}));
+
 vi.mock('file-saver', () => ({
   saveAs: (...args) => mockSaveAs(...args),
 }));
@@ -100,6 +108,21 @@ describe('AnnouncementItem', () => {
         await wrapper.vm.downloadAnnouncementResource(mockAnnouncementResource);
         expect(mockDownloadFile).not.toHaveBeenCalled();
         expect(mockSaveAs).toHaveBeenCalled();
+      });
+    });
+
+    describe('when downloadFile fails', () => {
+      it('shows a snackbar error', async () => {
+        const mockAnnouncementResource = {
+          announcement_resource_id: '123',
+        };
+        mockDownloadFile.mockRejectedValueOnce(new Error('mock error'));
+        await wrapper.vm.downloadAnnouncementResource(mockAnnouncementResource);
+        expect(pushNotificationErrorMock).toHaveBeenCalledWith(
+          'There is a problem with this link/file, please try again later. If the problem persists please contact the Gender Equity Office; paytransparency@gov.bc.ca',
+          '',
+          30000,
+        );
       });
     });
   });
