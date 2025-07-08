fc83de21c759829e092ecc1923647df33a470750
Sukanya Rath <sukanyarath1987@gmail.com>
2025-07-08 16:16:42 -0700

    adding in more to set the schema to be DB_Schema
    Signed-off-by: Sukanya Rath <sukanyarath1987@gmail.com>

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -15,7 +15,7 @@ const DB_SCHEMA = process.env.DB_SCHEMA || 'pay_transparency';
 const DB_CONNECTION_POOL_SIZE = process.env.DB_CONNECTION_POOL_SIZE || 5;
 
 const datasourceUrl = process.env.DATABASE_URL
-  ? `${process.env.DATABASE_URL}`
+  ? `${process.env.DATABASE_URL}?schema=${DB_SCHEMA}&pgbouncer=true`
   : `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=${DB_CONNECTION_POOL_SIZE}`;
 
 logger.silly(`Connecting to ${datasourceUrl}`);
