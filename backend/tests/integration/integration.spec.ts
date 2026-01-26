bdd4ac075af6bec10a3dc04cd9a3071c2d8798d3
jerekm <99355997+jerekm@users.noreply.github.com>
2026-01-26 15:10:09 -0800

    Integration test

---
+++ backend/tests/integration/integration.spec.ts
@@ -0,0 +1,27 @@
+import {
+  analyticsService,
+  PowerBiResourceName,
+} from '../../src/v1/services/analytic-service';
+
+// Integration tests to verify that the powerbi report
+// can be fetched successfully in each environment.
+describe('/ GET', () => {
+  it('should return 200 OK', async () => {
+    let info;
+    try {
+      info = await analyticsService.getEmbedInfo([
+        PowerBiResourceName.Analytics,
+      ]);
+    } catch (error) {
+      throw new Error(`Failed to get embed info: ${error}`);
+    }
+    expect(info).toHaveProperty('accessToken');
+    expect(info).toHaveProperty('expiry');
+    expect(info).toHaveProperty('resources');
+    expect(Array.isArray(info.resources)).toBe(true);
+    info.resources.forEach((resource) => {
+      expect(resource).toHaveProperty('id');
+      expect(resource).toHaveProperty('embedUrl');
+    });
+  });
+});
