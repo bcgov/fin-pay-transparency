32de5be4f48897b7036beb94ef40b9c43e17a242
jerekm <99355997+jerekm@users.noreply.github.com>
2026-02-25 12:56:04 -0800

    coverage

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      reporter: ['lcov', 'text-summary', 'text', 'json', 'html'],
+      exclude: ['src/**/index.ts'],
+    },
     projects: [
       {
         test: {
