430df9407c7291abbf95b879fcb624630041ca00
kardamk <kunal.kardam@gov.bc.ca>
2026-08-19 15:00:28 -0700

    feat: Geo-1387 Frontend reports table mobile compatibility

--- frontend/src/components/util/ReportsTable.vue
+++ frontend/src/components/util/ReportsTable.vue
@@ -1,6 +1,9 @@
 <template>
   <ReportSelectionManager />
+
+  <!-- Desktop / Tablet -->
   <v-data-table
+    v-if="!$vuetify.display.xs"
     :headers="headers"
     :items="reports"
     :items-per-page="3"
@@ -13,7 +16,7 @@
       <tr>
         <template v-for="column in columns" :key="column.key">
           <th v-if="column.key === 'report_url'" class="text-left pa-4">
-            <div class="d-flex flex-column align-start">
+            <div class="d-flex flex-column align-start font-weight-bold">
               <span>{{ column.title }}</span>
               <span
                 class="text-caption font-weight-regular text-none d-block opacity-80"
@@ -23,10 +26,15 @@
               </span>
             </div>
           </th>
-          <th v-else-if="column.key === 'actions'" class="text-center pa-4">
+
+          <th
+            v-else-if="column.key === 'actions'"
+            class="text-center pa-4 font-weight-bold"
+          >
             {{ column.title }}
           </th>
-          <th v-else class="text-left pa-4">
+
+          <th v-else class="text-left pa-4 font-weight-bold">
             {{ column.title }}
           </th>
         </template>
@@ -41,12 +49,14 @@
         >
           {{ item.reporting_year }}
         </td>
+
         <td
           :data-testid="`report_published_date-${item.report_id}`"
           class="text-start pa-4"
         >
           {{ formatDateTime(item.create_date) }}
         </td>
+
         <td class="pa-4 text-center">
           <div class="d-flex justify-center align-center">
             <v-btn
@@ -59,10 +69,11 @@
             >
               View
             </v-btn>
+
             <v-btn
               v-if="item.is_unlocked"
               :data-testid="`edit-report-${item.report_id}`"
-              prepend-icon="mdi-eye-outline"
+              prepend-icon="mdi-pencil-outline"
               variant="text"
               color="link"
               class="px-2"
@@ -72,10 +83,11 @@
             </v-btn>
           </div>
         </td>
+
         <td class="text-left pa-4">
           <template v-if="editingReportId === item.report_id">
             <div class="d-flex align-start w-100 ga-2">
-              <div class="flex-grow-1">
+              <div class="flex-grow-1 min-width-0">
                 <v-text-field
                   v-model="reportUrl"
                   density="compact"
@@ -112,6 +124,7 @@
               </div>
             </div>
           </template>
+
           <template v-else>
             <div class="d-flex align-center ga-2 flex-wrap">
               <template v-if="item.report_url">
@@ -150,6 +163,195 @@
       </tr>
     </template>
   </v-data-table>
+
+  <!-- Mobile -->
+  <v-container v-else fluid class="pa-0">
+    <v-card v-if="isLoading" variant="outlined" class="report-mobile-card">
+      <v-card-text class="d-flex justify-center py-6">
+        <v-progress-circular indeterminate color="primary" />
+      </v-card-text>
+    </v-card>
+
+    <v-card
+      v-else-if="reports.length === 0"
+      variant="outlined"
+      class="report-mobile-card"
+    >
+      <v-card-text class="text-center text-medium-emphasis">
+        No generated reports yet.
+      </v-card-text>
+    </v-card>
+
+    <div v-else class="d-flex flex-column ga-3 ma-1">
+      <v-card
+        v-for="item in reports"
+        :key="item.report_id"
+        variant="outlined"
+        class="report-mobile-card"
+      >
+        <v-card-text class="pa-0">
+          <!-- Column headings -->
+          <v-row no-gutters class="report-mobile-header px-3 py-2">
+            <v-col cols="4">
+              <span class="text-caption font-weight-bold">
+                Reporting Year
+              </span>
+            </v-col>
+
+            <v-col cols="4">
+              <span class="text-caption font-weight-bold">
+                Submission Date
+              </span>
+            </v-col>
+
+            <v-col cols="4" class="text-center">
+              <span class="text-caption font-weight-bold"> Action </span>
+            </v-col>
+          </v-row>
+
+          <!-- Report information -->
+          <v-row no-gutters class="px-3 pt-2 pa-1">
+            <v-col cols="4">
+              <div
+                :data-testid="`reporting_year-${item.report_id}`"
+                class="text-body-2"
+              >
+                {{ item.reporting_year }}
+              </div>
+            </v-col>
+
+            <v-col cols="4">
+              <div
+                :data-testid="`report_published_date-${item.report_id}`"
+                class="text-body-2"
+              >
+                {{ formatDateTime(item.create_date) }}
+              </div>
+            </v-col>
+
+            <v-col cols="4" class="d-flex justify-content align-start">
+              <div class="d-flex align-center">
+                <v-btn
+                  :data-testid="`view-report-${item.report_id}`"
+                  prepend-icon="mdi-eye-outline"
+                  variant="text"
+                  color="link"
+                  size="small"
+                  class="px-1"
+                  @click="viewReport(item)"
+                >
+                  View
+                </v-btn>
+
+                <v-btn
+                  v-if="item.is_unlocked"
+                  :data-testid="`edit-report-${item.report_id}`"
+                  prepend-icon="mdi-pencil-outline"
+                  variant="text"
+                  color="link"
+                  size="small"
+                  class="px-1"
+                  @click="editReport(item)"
+                >
+                  Edit
+                </v-btn>
+              </div>
+            </v-col>
+          </v-row>
+
+          <!-- Published report URL -->
+          <v-divider class="mt-2" />
+
+          <div class="px-3 py-3">
+            <div class="text-caption font-weight-bold mb-2">
+              Link to published report
+            </div>
+
+            <div class="text-caption text-medium-emphasis mb-3">
+              We invite you to share your most recent report link with our
+              office for compliance tracking
+            </div>
+
+            <!-- Editing URL -->
+            <template v-if="editingReportId === item.report_id">
+              <div class="d-flex flex-column ga-2">
+                <v-text-field
+                  v-model="reportUrl"
+                  density="compact"
+                  variant="outlined"
+                  hide-details
+                  class="w-100"
+                  :error="!!errorMessage"
+                  placeholder="https://example.gov.bc.ca/reports"
+                />
+
+                <div v-if="errorMessage" class="text-error text-caption">
+                  {{ errorMessage }}
+                </div>
+
+                <div class="d-flex justify-end align-center ga-1">
+                  <v-btn
+                    color="primary"
+                    size="small"
+                    @click="saveReportUrl(item)"
+                  >
+                    Save
+                  </v-btn>
+
+                  <v-btn
+                    icon="mdi-close"
+                    variant="text"
+                    size="small"
+                    @click="cancelEditingReportUrl"
+                  />
+                </div>
+              </div>
+            </template>
+
+            <!-- Existing URL -->
+            <template v-else>
+              <template v-if="item.report_url">
+                <div class="d-flex align-start ga-2">
+                  <a
+                    :href="item.report_url"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="text-body-2 text-decoration-underline flex-grow-1"
+                  >
+                    {{ item.report_url }}
+                  </a>
+
+                  <v-btn
+                    color="link"
+                    variant="text"
+                    size="small"
+                    prepend-icon="mdi-pencil"
+                    class="flex-shrink-0 px-1"
+                    @click="startEditingReportUrl(item)"
+                  >
+                    Edit Link
+                  </v-btn>
+                </div>
+              </template>
+
+              <v-btn
+                v-else
+                color="link"
+                variant="text"
+                size="small"
+                prepend-icon="mdi-link-variant"
+                class="px-0"
+                @click="startEditingReportUrl(item)"
+              >
+                +Add link to published report
+              </v-btn>
+            </template>
+          </div>
+        </v-card-text>
+      </v-card>
+    </div>
+  </v-container>
+
   <ConfirmationDialog ref="confirmClearUrlDialog" />
 </template>
 
